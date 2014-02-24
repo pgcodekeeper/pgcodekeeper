a08d0c2f139e7e740ab319ed8ab08d74bb7398d1
Левша Александр <levsha_aa@taximaxim.ru>
2014-02-24 16:58:13 +0400

    Базовый граф объектов БД.

---
+++ src/main/java/ru/taximaxim/codekeeper/apgdiff/model/graph/DepcyGraph.java
@@ -0,0 +1,88 @@
+package ru.taximaxim.codekeeper.apgdiff.model.graph;
+
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleDirectedGraph;
+
+import cz.startnet.utils.pgdiff.schema.PgConstraint;
+import cz.startnet.utils.pgdiff.schema.PgDatabase;
+import cz.startnet.utils.pgdiff.schema.PgExtension;
+import cz.startnet.utils.pgdiff.schema.PgFunction;
+import cz.startnet.utils.pgdiff.schema.PgIndex;
+import cz.startnet.utils.pgdiff.schema.PgSchema;
+import cz.startnet.utils.pgdiff.schema.PgSequence;
+import cz.startnet.utils.pgdiff.schema.PgStatement;
+import cz.startnet.utils.pgdiff.schema.PgTable;
+import cz.startnet.utils.pgdiff.schema.PgTrigger;
+import cz.startnet.utils.pgdiff.schema.PgView;
+
+public class DepcyGraph {
+    
+    /**
+     * Направление связей в графе:<br>
+     * зависимость <-- зависящий объект
+     */
+    private final DirectedGraph<PgStatement, DefaultEdge> graph = 
+            new SimpleDirectedGraph<>(DefaultEdge.class);
+            
+    /**
+     * Copied database, graph source.<br>
+     * <b>Do not modify</b> any elements in this as it will break 
+     * HashSets/HashMaps and with them the generated graph.
+     */
+    private final PgDatabase db;
+    
+    public DepcyGraph(PgDatabase graphSrc) {
+        db = graphSrc.deepCopy();
+        create();
+    }
+    
+    private void create() {
+        graph.addVertex(db);
+        
+        for(PgSchema schema : db.getSchemas()) {
+            graph.addVertex(schema);
+            graph.addEdge(schema, db);
+            
+            for(PgFunction func : schema.getFunctions()) {
+                graph.addVertex(func);
+                graph.addEdge(func, schema);
+            }
+            
+            for(PgSequence seq : schema.getSequences()) {
+                graph.addVertex(seq);
+                graph.addEdge(seq, schema);
+            }
+            
+            for(PgTable table : schema.getTables()) {
+                graph.addVertex(table);
+                graph.addEdge(table, schema);
+                
+                for(PgIndex idx : table.getIndexes()) {
+                    graph.addVertex(idx);
+                    graph.addEdge(idx, table);
+                }
+                
+                for(PgConstraint cons : table.getConstraints()) {
+                    graph.addVertex(cons);
+                    graph.addEdge(cons, table);
+                }
+                
+                for(PgTrigger trig : table.getTriggers()) {
+                    graph.addVertex(trig);
+                    graph.addEdge(trig, table);
+                }
+            }
+            
+            for(PgView view : schema.getViews()) {
+                graph.addVertex(view);
+                graph.addEdge(view, schema);
+            }
+        }
+        
+        for(PgExtension ext : db.getExtensions()) {
+            graph.addVertex(ext);
+            graph.addEdge(ext, db);
+        }
+    }
+}
