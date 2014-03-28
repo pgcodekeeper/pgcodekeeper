0305415a7cf271c88acae146c0f8fe88bcf64b2f
Anton Ryabinin <ryabinin_av@taximaxim.ru>
2014-03-28 16:02:46 +0400

    Changed JGit init class to work with JGit

--- src/ru/taximaxim/codekeeper/ui/tests/TestJGit.java
+++ src/ru/taximaxim/codekeeper/ui/tests/TestJGit.java
@@ -4,6 +4,8 @@ package ru.taximaxim.codekeeper.ui.tests;
 import java.io.File;
 import java.nio.file.Files;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.InitCommand;
 import org.junit.Before;
 
 import ru.taximaxim.codekeeper.ui.externalcalls.JGitExec;
@@ -12,16 +14,18 @@ public class TestJGit extends TestIRepoWorker{
     
     @Before
     public void setUp() throws Exception {
-        //TODO Testing still relies on external git
         pathToOrigin = Files.createTempDirectory("");
         copyFilesToPath(pathToOrigin);
         // init a git repo at pathToOrigin
         File dirRepo = new File (pathToOrigin.toString());
-        runRepoBinary("git", dirRepo, "init");
-        runRepoBinary("git", dirRepo, "add", ".");
-        runRepoBinary("git", dirRepo, "commit", "-m", "initial");
+        InitCommand initCom = Git.init();
+        initCom.setDirectory(dirRepo).call();
+        Git git = Git.open(dirRepo);
+        git.add().addFilepattern(".").call();
+        git.commit().setMessage("init").setAll(true).call();
+        //TODO replace external git call
         runRepoBinary("git", dirRepo, "config", "--bool", "core.bare", "true");
-        repo = new JGitExec(pathToOrigin.toString(), "", "");
+        repo = new JGitExec(pathToOrigin.toString(), "", "", "");
         pathToWorking = Files.createTempDirectory("");
     }
 }
