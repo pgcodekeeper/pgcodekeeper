7832e72ff6443b82e724f8b3a056b2ca05249bae
Левша Александр <levsha_aa@taximaxim.ru>
2014-03-31 15:49:43 +0400

    ReadOnlyFileRemover NPE fix

--- src/ru/taximaxim/codekeeper/ui/fileutils/ReadOnlyFileRemover.java
+++ src/ru/taximaxim/codekeeper/ui/fileutils/ReadOnlyFileRemover.java
@@ -2,15 +2,13 @@ package ru.taximaxim.codekeeper.ui.fileutils;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.attribute.DosFileAttributeView;
 
 public class ReadOnlyFileRemover {
     
     public static void remove(Path path) throws IOException {
-        Files.getFileAttributeView(path, DosFileAttributeView.class,
-                (LinkOption) null).setReadOnly(false);
+        Files.getFileAttributeView(path, DosFileAttributeView.class).setReadOnly(false);
         Files.delete(path);
     }
 }
