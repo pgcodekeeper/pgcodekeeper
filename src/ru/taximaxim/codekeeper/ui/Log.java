70268b6313d49eeede9fed545d985619739fd8c6
Anton Ryabinin <ryabinin_av@taximaxim.ru>
2014-04-01 10:23:17 +0400

    New branch 'log' Added LogService variable to Log class

--- src/ru/taximaxim/codekeeper/ui/Log.java
+++ src/ru/taximaxim/codekeeper/ui/Log.java
@@ -1,9 +1,11 @@
 package ru.taximaxim.codekeeper.ui;
 
 import org.eclipse.e4.core.services.log.Logger;
+import org.osgi.service.log.LogService;
 
 public class Log {
     
+    private LogService s;
     private static Logger log = null;
     
     public static Logger getLog() {
