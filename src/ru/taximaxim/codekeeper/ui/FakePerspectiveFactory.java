f719d9850156e0ec09878424fd39bfa668aded94
Левша Александр <levsha_aa@taximaxim.ru>
2014-05-22 11:52:52 +0400

    иконки перспектив заглушки перспектив из модели e4xmi объявлены в org.eclipse.ui.perspectives extension point пункт меню Show Perspective хэндлится стандартным платформенным хэндлером хоткей Ctrl+Q удален

---
+++ src/ru/taximaxim/codekeeper/ui/FakePerspectiveFactory.java
@@ -0,0 +1,12 @@
+package ru.taximaxim.codekeeper.ui;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class FakePerspectiveFactory implements IPerspectiveFactory {
+
+    @Override
+    public void createInitialLayout(IPageLayout layout) {
+        // nothing, defined in e4xmi
+    }
+}
