499656c4016fdae41949e471c2595aef81c08a68
ryabinin_av <ryabinin_av@taximaxim.ru>
2014-05-30 13:30:08 +0400

    Marker file creation in ModelExporter

---
+++ src/main/java/ru/taximaxim/codekeeper/apgdiff/ApgdiffConsts.java
@@ -0,0 +1,10 @@
+package ru.taximaxim.codekeeper.apgdiff;
+
+/**
+ * Stores string constants
+ * 
+ * @author Anton Ryabinin
+ */
+public interface ApgdiffConsts {
+    String FILENAME_WORKING_DIR_MARKER = ".pgcodekeeper";
+}
