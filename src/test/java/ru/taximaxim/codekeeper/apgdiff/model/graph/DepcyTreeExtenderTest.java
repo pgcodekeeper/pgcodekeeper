ac915413fcf18bee67a0e598a9b0f58d39fca579
ryabinin_av <ryabinin_av@taximaxim.ru>
2014-08-12 15:56:43 +0400

    test stubs

---
+++ src/test/java/ru/taximaxim/codekeeper/apgdiff/model/graph/DepcyTreeExtenderTest.java
@@ -0,0 +1,34 @@
+package ru.taximaxim.codekeeper.apgdiff.model.graph;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DepcyTreeExtenderTest {
+
+    @Test
+    public void testGetDependenciesOfNew() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetTreeCopyWithDepcy() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetDepcyTargetDb() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSumAllDepcies() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetConflicting() {
+        fail("Not yet implemented");
+    }
+
+}
