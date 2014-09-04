6a5d2ac4e444fd76b67854652589dc17b8917422
Левша Александр <levsha_aa@taximaxim.ru>
2014-09-04 15:56:06 +0400

    перенес \n

--- src/main/java/cz/startnet/utils/pgdiff/schema/PgTable.java
+++ src/main/java/cz/startnet/utils/pgdiff/schema/PgTable.java
@@ -219,25 +219,24 @@ public class PgTable extends PgStatementWithSearchPath {
     @Override
     public String getFullSQL() {
         final StringBuilder sbSQL = new StringBuilder(10);
-        sbSQL.append(getCreationSQL());
-        sbSQL.append(getClusterSQL());
+        sbSQL.append(getCreationSQL()).append("\n\n").append(getClusterSQL());
         
         return sbSQL.toString();
     }
     
     @Override
     public String getDropSQL() {
-        return "DROP TABLE " + PgDiffUtils.getQuotedName(getName()) + ";";
+        return "DROP TABLE " + PgDiffUtils.getQuotedName(getName()) + ';';
     }
     
     private String getClusterSQL() {
         final StringBuilder sbSQL = new StringBuilder(10);
         if (clusterIndexName != null && !clusterIndexName.isEmpty()) {
-            sbSQL.append("\n\nALTER TABLE ");
+            sbSQL.append("ALTER TABLE ");
             sbSQL.append(PgDiffUtils.getQuotedName(name));
             sbSQL.append(" CLUSTER ON ");
             sbSQL.append(getClusterIndexName());
-            sbSQL.append(";");
+            sbSQL.append(';');
         }
         return sbSQL.toString();
     }
