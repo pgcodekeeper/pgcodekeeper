8a309376f6f67e818b6b8bf2754667c3e0c80190
ryabinin_av <ryabinin_av@taximaxim.ru>
2014-10-09 15:22:28 +0400

    Bug fix: setting inherited table name keeps possible schema-qualificat

--- src/main/java/cz/startnet/utils/pgdiff/parsers/CreateTableParser.java
+++ src/main/java/cz/startnet/utils/pgdiff/parsers/CreateTableParser.java
@@ -98,8 +98,7 @@ public class CreateTableParser {
         parser.expect("(");
 
         while (!parser.expectOptional(")")) {
-            table.addInherits(
-                    ParserUtils.getObjectName(parser.parseIdentifier()));
+            table.addInherits(parser.parseIdentifier());
 
             if (parser.expectOptional(")")) {
                 break;
