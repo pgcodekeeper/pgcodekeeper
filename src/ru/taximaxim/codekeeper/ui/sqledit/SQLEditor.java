1228ba68bfec6c4dcd74f4a644445b9894bf005b
Ботов Анатолий <botov_av@taximaxim.ru>
2014-10-29 12:07:35 +0400

    Автозавершение, всплывающие подсказки, вывод выражений в OutLine View.

--- src/ru/taximaxim/codekeeper/ui/sqledit/SQLEditor.java
+++ src/ru/taximaxim/codekeeper/ui/sqledit/SQLEditor.java
@@ -1,8 +1,121 @@
 package ru.taximaxim.codekeeper.ui.sqledit;
 
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.projection.Segment;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.texteditor.AbstractDecoratedTextEditor;
+import org.eclipse.ui.texteditor.IDocumentProvider;
+import org.eclipse.ui.views.contentoutline.ContentOutlinePage;
+import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
 
 public class SQLEditor extends AbstractDecoratedTextEditor {
+
+    // разобраться с вычислением частей документа и выводить части в аутлайн
+    private final class MyContentOutlinePage extends ContentOutlinePage {
+        private IEditorInput fInput;
+        private AbstractDecoratedTextEditor fTextEditor;
+        private IDocumentProvider fDocumentProvider;
+
+        public MyContentOutlinePage(IDocumentProvider fDocumentProvider,
+                AbstractDecoratedTextEditor sqlEditor) {
+            fTextEditor = sqlEditor;
+            this.fDocumentProvider = fDocumentProvider;
+        }
+
+        public void setInput(IEditorInput input) {
+            this.fInput = input;
+        }
+
+        @Override
+        public void createControl(Composite parent) {
+            super.createControl(parent);
+            TreeViewer viewer = getTreeViewer();
+            viewer.setContentProvider(new ITreeContentProvider() {
+
+                @Override
+                public void inputChanged(Viewer viewer, Object oldInput,
+                        Object newInput) {
+                    if (newInput != null) {
+                        IDocument document = fDocumentProvider
+                                .getDocument(newInput);
+                        if (document != null) {
+                            // document.addPositionCategory(SEGMENTS);
+                            // document.addPositionUpdater(fPositionUpdater);
+                            // parse(document);
+                        }
+                    }
+                }
+
+                @Override
+                public void dispose() {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public Object[] getElements(Object inputElement) {
+                    // TODO Auto-generated method stub
+                    return new Segment[] { new Segment(4, 6),
+                            new Segment(50, 6) };
+                }
+
+                @Override
+                public Object[] getChildren(Object parentElement) {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public Object getParent(Object element) {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public boolean hasChildren(Object element) {
+                    // TODO Auto-generated method stub
+                    return false;
+                }
+            });
+
+            viewer.setLabelProvider(new LabelProvider());
+            viewer.addSelectionChangedListener(this);
+
+            if (fInput != null)
+                viewer.setInput(fInput);
+
+        }
+
+        @Override
+        public void selectionChanged(SelectionChangedEvent event) {
+            super.selectionChanged(event);
+
+            ISelection selection = event.getSelection();
+            if (selection.isEmpty())
+                fTextEditor.resetHighlightRange();
+            else {
+                Segment segment = (Segment) ((IStructuredSelection) selection)
+                        .getFirstElement();
+                int start = segment.getOffset();
+                int length = segment.getLength();
+                try {
+                    fTextEditor.setHighlightRange(start, length, true);
+                } catch (IllegalArgumentException x) {
+                    fTextEditor.resetHighlightRange();
+                }
+            }
+        }
+    }
+
+    private MyContentOutlinePage fOutlinePage;
 
     public SQLEditor() {
         super();
@@ -10,4 +123,16 @@ public class SQLEditor extends AbstractDecoratedTextEditor {
         setDocumentProvider(new SQLDocumentProvider());
     }
 
+    @Override
+    public Object getAdapter(Class adapter) {
+        if (IContentOutlinePage.class.equals(adapter)) {
+            if (fOutlinePage == null) {
+                fOutlinePage= new MyContentOutlinePage(getDocumentProvider(), this);
+                if (getEditorInput() != null)
+                    fOutlinePage.setInput(getEditorInput());
+            }
+            return fOutlinePage;
+        }
+        return super.getAdapter(adapter);
+    }
 }
--- src/ru/taximaxim/codekeeper/ui/sqledit/SQLSourceViewerConfiguration.java
+++ src/ru/taximaxim/codekeeper/ui/sqledit/SQLSourceViewerConfiguration.java
@@ -1,7 +1,24 @@
 package ru.taximaxim.codekeeper.ui.sqledit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITextHover;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.Region;
 import org.eclipse.jface.text.TextAttribute;
+import org.eclipse.jface.text.contentassist.CompletionProposal;
+import org.eclipse.jface.text.contentassist.ContentAssistant;
+import org.eclipse.jface.text.contentassist.ContextInformation;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.jface.text.contentassist.IContentAssistProcessor;
+import org.eclipse.jface.text.contentassist.IContentAssistant;
+import org.eclipse.jface.text.contentassist.IContextInformation;
+import org.eclipse.jface.text.contentassist.IContextInformationValidator;
 import org.eclipse.jface.text.presentation.IPresentationReconciler;
 import org.eclipse.jface.text.presentation.PresentationReconciler;
 import org.eclipse.jface.text.rules.DefaultDamagerRepairer;
@@ -10,10 +27,12 @@ import org.eclipse.jface.text.rules.IWordDetector;
 import org.eclipse.jface.text.rules.RuleBasedScanner;
 import org.eclipse.jface.text.rules.Token;
 import org.eclipse.jface.text.rules.WordRule;
+import org.eclipse.jface.text.source.IAnnotationHover;
 import org.eclipse.jface.text.source.ISharedTextColors;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.ui.editors.text.TextSourceViewerConfiguration;
 
@@ -30,16 +49,122 @@ public class SQLSourceViewerConfiguration extends TextSourceViewerConfiguration 
     }
     
     private final ISharedTextColors fSharedColors;
+    private final SqlPostgresSyntax sqlSyntax = new SqlPostgresSyntax();;
     
     public SQLSourceViewerConfiguration(ISharedTextColors sharedColors, IPreferenceStore store) {
         super(store);
         fSharedColors= sharedColors;
     }
+    
+    // Всплывающая подсказка пока
+    @Override
+    public ITextHover getTextHover(ISourceViewer sourceViewer,
+            String contentType) {
+        return new ITextHover() {
+            
+            @Override
+            public IRegion getHoverRegion(ITextViewer textViewer, int offset) {
+                Point selection= textViewer.getSelectedRange();
+                if (selection.x <= offset && offset < selection.x + selection.y)
+                    return new Region(selection.x, selection.y);
+                return new Region(offset, 0);
+            }
+            
+            @Override
+            public String getHoverInfo(ITextViewer textViewer, IRegion hoverRegion) {
+                if (hoverRegion != null) {
+                    try {
+                        if (hoverRegion.getLength() > -1)
+                            return textViewer.getDocument().get(
+                                    hoverRegion.getOffset(),
+                                    hoverRegion.getLength());
+                    } catch (BadLocationException x) {
+                    }
+                }
+                return "JavaTextHover.emptySelection"; 
+            }
+        };
+    }
+    
+    // Отображает всю строку при наведении на левую полосу редактора
+    @Override
+    public IAnnotationHover getAnnotationHover(ISourceViewer sourceViewer) {
+        return new IAnnotationHover() {
+            
+            @Override
+            public String getHoverInfo(ISourceViewer sourceViewer, int lineNumber) {
+                IDocument document= sourceViewer.getDocument();
 
+                try {
+                    IRegion info= document.getLineInformation(lineNumber);
+                    return document.get(info.getOffset(), info.getLength());
+                } catch (BadLocationException x) {
+                }
+                return null;
+            }
+        };
+    }
+    
+    // Автозавершение, не работает при вводе не фильтрует по вводу
+    @Override
+    public IContentAssistant getContentAssistant(ISourceViewer sourceViewer) {
+        ContentAssistant assistant= new ContentAssistant();
+        assistant.setContentAssistProcessor(new IContentAssistProcessor() {
+            
+            @Override
+            public String getErrorMessage() {
+                return "Error Message";
+            }
+            
+            @Override
+            public IContextInformationValidator getContextInformationValidator() {
+                return null;
+            }
+            
+            @Override
+            public char[] getContextInformationAutoActivationCharacters() {
+                return new char[] { '#' };
+            }
+            
+            @Override
+            public char[] getCompletionProposalAutoActivationCharacters() {
+                return new char[] { '.', '(' };
+            }
+            
+            @Override
+            public IContextInformation[] computeContextInformation(ITextViewer viewer,
+                    int offset) {
+                IContextInformation[] result= new IContextInformation[5];
+                for (int i= 0; i < result.length; i++)
+                    result[i]= new ContextInformation("CompletionProcessor.ContextInfo.display.pattern " + i + " " + offset,
+                        "CompletionProcessor.ContextInfo.value.pattern " + i + " " + (offset - 5) + " " +(offset + 5));
+                return result;
+            }
+            
+            @Override
+            public ICompletionProposal[] computeCompletionProposals(ITextViewer viewer,
+                    int offset) {
+                List<ICompletionProposal> result= new ArrayList<ICompletionProposal>();
+                for (String fgProposal : sqlSyntax.getReservedwords()) {
+                    IContextInformation info = new ContextInformation(fgProposal, fgProposal);
+                    result.add(new CompletionProposal(fgProposal, offset, 0, fgProposal.length(), null, fgProposal, info, fgProposal));
+                }
+                return result.toArray(new ICompletionProposal[result.size()]);
+            }
+        }, IDocument.DEFAULT_CONTENT_TYPE);
+        assistant.enableAutoActivation(true);
+        assistant.setAutoActivationDelay(500);
+        assistant.setProposalPopupOrientation(IContentAssistant.PROPOSAL_OVERLAY);
+        assistant.setContextInformationPopupOrientation(IContentAssistant.CONTEXT_INFO_ABOVE);
+        return assistant;
+    }
+
+    @Override
     public String getConfiguredDocumentPartitioning(ISourceViewer sourceViewer) {
         return SQLDocumentProvider.SQL_PARTITIONING;
     }
     
+    @Override
     public String[] getConfiguredContentTypes(ISourceViewer sourceViewer) {
         return new String[] {
                 SQLDocumentProvider.SQL_CODE,
@@ -47,6 +172,7 @@ public class SQLSourceViewerConfiguration extends TextSourceViewerConfiguration 
         };
     }
     
+    @Override
     public IPresentationReconciler getPresentationReconciler(ISourceViewer sourceViewer) {
         PresentationReconciler reconciler= new PresentationReconciler();
         reconciler.setDocumentPartitioning(getConfiguredDocumentPartitioning(sourceViewer));
@@ -75,7 +201,6 @@ public class SQLSourceViewerConfiguration extends TextSourceViewerConfiguration 
     }
 
     private WordRule sqlSyntaxRules() {
-        SqlPostgresSyntax sqlSyntax = new SqlPostgresSyntax();
         
         // Define a word rule and add SQL keywords to it.
         WordRule wordRule = new WordRule(new WordDetector(), Token.WHITESPACE);
