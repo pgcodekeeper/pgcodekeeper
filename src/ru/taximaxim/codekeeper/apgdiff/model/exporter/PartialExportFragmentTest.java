efb98a53899715868ffe8a25fb2d3f27b4f591b8
ryabinin_av <ryabinin_av@taximaxim.ru>
2014-12-09 17:32:18 +0300

    PartialExportTest wrapper

---
+++ src/ru/taximaxim/codekeeper/apgdiff/model/exporter/PartialExportFragmentTest.java
@@ -0,0 +1,8 @@
+package ru.taximaxim.codekeeper.apgdiff.model.exporter;
+
+public class PartialExportFragmentTest extends PartialExporterTest {
+
+    public PartialExportFragmentTest(int index) {
+        super(index);
+    }
+}
