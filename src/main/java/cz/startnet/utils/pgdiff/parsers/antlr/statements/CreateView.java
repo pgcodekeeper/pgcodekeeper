451aecaf49d94930378d531ebf69e0487293a1d1
Ботов Анатолий <botov_av@taximaxim.ru>
2014-12-12 16:54:32 +0300

    Разбор View, некоторые вьюхи парсятся более подробно чем с апгдиффом, остальные нужно проверить на тестах.

--- src/main/java/cz/startnet/utils/pgdiff/parsers/antlr/statements/CreateView.java
+++ src/main/java/cz/startnet/utils/pgdiff/parsers/antlr/statements/CreateView.java
@@ -9,46 +9,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
-
 import cz.startnet.utils.pgdiff.parsers.ParserUtils;
 import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.As_clauseContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Column_referencesContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Common_value_expressionContext;
 import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Create_view_statementContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Cross_joinContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Derived_columnContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Explicit_tableContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.From_clauseContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Join_conditionContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Join_specificationContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Joined_tableContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Joined_table_primaryContext;
 import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Name_or_func_callsContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Named_columns_joinContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Natural_joinContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Non_join_query_expressionContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Non_join_query_primaryContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Non_join_query_termContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Nonparenthesized_value_expression_primaryContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Numeric_value_expressionContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Parenthesized_value_expressionContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Qualified_joinContext;
 import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Query_expressionContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Query_primaryContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Query_specificationContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Query_termContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Select_listContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Select_sublistContext;
+import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Set_function_specificationContext;
 import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Simple_tableContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Table_expressionContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Table_primaryContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Table_referenceContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Table_subqueryContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Value_expressionContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Value_expression_primaryContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Value_expression_primary_castContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.SQLParserBaseListener;
+import cz.startnet.utils.pgdiff.parsers.antlr.SQLParserBaseVisitor;
 import cz.startnet.utils.pgdiff.schema.GenericColumn;
 import cz.startnet.utils.pgdiff.schema.PgDatabase;
 import cz.startnet.utils.pgdiff.schema.PgSelect;
@@ -87,92 +55,90 @@ public class CreateView extends ParserAbstract {
 
     private PgSelect parseSelect(Query_expressionContext ctx) {
         PgSelect select = new PgSelect(getFullCtxText(ctx), null);
-        ParseTreeWalker walker = new ParseTreeWalker();
-        SelectListener sleList = new SelectListener(select);
-        walker.walk(sleList, ctx);
-        return sleList.getSelect();
+        MyVisitor visitor = new MyVisitor(select);
+        visitor.visit(ctx);
+        return visitor.getSelect();
     }
 
-    private class SelectListener extends SQLParserBaseListener {
+    private class MyVisitor extends
+            SQLParserBaseVisitor<Query_expressionContext> {
+        // список алиасов запросов, игнорируются при заполнении колонок в селект
+        private Queue<String> aliasNames = new LinkedList<>();
         private List<GenericColumn> columns = new ArrayList<>();
         private Map<String, String> tableAliases = new HashMap<>();
-        private Queue<String> aliasNames = new LinkedList<>();
+        boolean isQiery = false;
         private PgSelect select;
-        private boolean isFrom = false;
-        private boolean isFromQuery = false;
 
-        public SelectListener(PgSelect select) {
+        public MyVisitor(PgSelect select) {
             this.select = select;
         }
 
         @Override
-        public void enterTable_primary(Table_primaryContext ctx) {
-            if (ctx.as_clause() != null) {
-                aliasNames.add(ctx.as_clause().identifier().getText());
+        public Query_expressionContext visitSimple_table(Simple_tableContext ctx) {
+            if (ctx.query_specification() != null) {
+                MyVisitor vis = new MyVisitor(select);
+                vis.visit(ctx.query_specification());
+                isQiery = true;
+                aliasNames.addAll(vis.aliasNames);
+                columns.addAll(vis.columns);
+                tableAliases.putAll(vis.tableAliases);
+                return null;
             }
+            return super.visitSimple_table(ctx);
         }
 
-//        @Override
-//        public void enterDerived_column(Derived_columnContext ctx) {
-//            for (As_clauseContext asCtx : ctx.as_clause()) {
-//                aliasNames.add(asCtx.identifier().getText());
-//            }
-//        }
+        @Override
+        public Query_expressionContext visitSet_function_specification(
+                Set_function_specificationContext ctx) {
+            if (ctx.COUNT() != null) {
+                columns.add(new GenericColumn(ctx.COUNT().getText()));
+            } else {
+                columns.add(new GenericColumn(ctx.general_set_function()
+                        .set_function_type().getText()));
+            }
+            return null;
+        }
 
         @Override
-        public void enterName_or_func_calls(Name_or_func_callsContext ctx) {
+        public Query_expressionContext visitName_or_func_calls(
+                Name_or_func_callsContext ctx) {
+            String colName;
             if (ctx.function_calls_paren() != null) {
-                return;
+                colName = getFullCtxText(ctx);
+            } else {
+                colName = getName(ctx.schema_qualified_name());
             }
-            String colName = getName(ctx.schema_qualified_name());
+
             String colTable = getTableName(ctx.schema_qualified_name());
             String colSchema = getSchemaName(ctx.schema_qualified_name());
-            if (!isFrom) {
-                if (colSchema == null 
-                        || colSchema.equals(colTable)) {
-                    columns.add(new GenericColumn(null, colTable, colName));
-                } else {
-                    columns.add(new GenericColumn(colSchema, colTable, colName));
+            if (colSchema == null || colSchema.equals(colTable)) {
+                columns.add(new GenericColumn(null, colTable, colName));
+            } else {
+                columns.add(new GenericColumn(colSchema, colTable, colName));
+            }
+            return null;
+        }
+
+        @Override
+        public Query_expressionContext visitAs_clause(As_clauseContext ctx) {
+            String aliasName = ctx.identifier().getText();
+            if (isQiery) {
+                isQiery = false;
+                aliasNames.add(aliasName);
+                Iterator<GenericColumn> iter = columns.iterator();
+                while (iter.hasNext()) {
+                    GenericColumn col = iter.next();
+                    if (col.table != null && col.table.equals(aliasName)) {
+                        iter.remove();
+                    }
                 }
             } else {
-                if (isFromQuery) {
-                    Iterator<GenericColumn> iter = columns.iterator();
-                    while (iter.hasNext()) {
-                        GenericColumn col = iter.next();
-                        if (col.table.equals(aliasNames.peek())) {
-                            iter.remove();
-                        }
-                    }
-                } else {
-                    tableAliases.put(aliasNames.poll(),
-                            getFullCtxText(ctx.schema_qualified_name()));
-                }
-                
+                tableAliases.put(aliasName,
+                        columns.get(columns.size() - 1).column);
             }
+            return super.visitAs_clause(ctx);
         }
 
-        @Override
-        public void enterFrom_clause(From_clauseContext ctx) {
-            isFrom = true;
-        }
-
-        @Override
-        public void exitFrom_clause(From_clauseContext ctx) {
-            isFrom = false;
-        }
-        @Override
-        public void enterQuery_specification(Query_specificationContext ctx) {
-            if (isFrom) {
-                isFromQuery  =true;
-            }
-        };
-        @Override
-        public void exitQuery_specification(Query_specificationContext ctx) {
-            if (isFrom) {
-                isFromQuery = false;
-            }
-        };
-        
         PgSelect getSelect() {
             for (GenericColumn column : columns) {
                 if (column.schema == null && column.table != null) {
@@ -183,8 +149,16 @@ public class CreateView extends ParserAbstract {
                                 ParserUtils.getObjectName(unaliased),
                                 column.column);
                     }
+                    // те колонки которые не попали в алиасы не должны быть в
+                    // списке алиасов подзапросов
+                    if (!aliasNames.contains(column.table)) {
+                        column = new GenericColumn(
+                                column.schema != null ? column.schema : db
+                                        .getDefaultSchema().getName(),
+                                column.table, column.column);
+                        select.addColumn(column);
+                    }
                 }
-                select.addColumn(column);
             }
             return select;
         }
