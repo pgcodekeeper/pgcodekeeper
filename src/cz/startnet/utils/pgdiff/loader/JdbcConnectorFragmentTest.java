8c2d27fbb3219e76365110f17021f1b3667b4595
ryabinin_av <ryabinin_av@taximaxim.ru>
2014-12-16 15:00:33 +0300

    jdbc connector+pgpass parser test

---
+++ src/cz/startnet/utils/pgdiff/loader/JdbcConnectorFragmentTest.java
@@ -0,0 +1,9 @@
+package cz.startnet.utils.pgdiff.loader;
+
+public class JdbcConnectorFragmentTest extends JdbcConnectorTest {
+
+    public JdbcConnectorFragmentTest() {
+        super();
+    }
+
+}
