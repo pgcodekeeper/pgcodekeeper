f570775486e9e7b9333aaa9da756882002878560
Левша Александр <levsha_aa@taximaxim.ru>
2014-12-19 17:24:11 +0400

    фрагмент тест antlr парсера

---
+++ src/cz/startnet/utils/pgdiff/parsers/antlr/PgAntlrLoaderFragmentTest.java
@@ -0,0 +1,8 @@
+package cz.startnet.utils.pgdiff.parsers.antlr;
+
+public class PgAntlrLoaderFragmentTest extends PgAntlrLoaderTest {
+
+    public PgAntlrLoaderFragmentTest(int fileIndex) {
+        super(fileIndex);
+    }
+}
