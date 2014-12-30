358623cf3a0b983346625e7ffe0396cb592e50f4
Ботов Анатолий <botov_av@taximaxim.ru>
2014-12-30 16:25:45 +0300

    Alter sequence owned by, закостылено созданием новой таблицы, чтобы не писать новый метод.

--- src/main/java/cz/startnet/utils/pgdiff/parsers/antlr/statements/AlterSequence.java
+++ src/main/java/cz/startnet/utils/pgdiff/parsers/antlr/statements/AlterSequence.java
@@ -7,6 +7,7 @@ import cz.startnet.utils.pgdiff.parsers.antlr.SQLParser.Sequence_bodyContext;
 import cz.startnet.utils.pgdiff.schema.PgDatabase;
 import cz.startnet.utils.pgdiff.schema.PgSequence;
 import cz.startnet.utils.pgdiff.schema.PgStatement;
+import cz.startnet.utils.pgdiff.schema.PgTable;
 
 public class AlterSequence extends ParserAbstract {
 private Alter_sequence_statementContext ctx;
@@ -33,6 +34,13 @@ private Alter_sequence_statementContext ctx;
         for (Sequence_bodyContext seqbody : ctx.sequence_body()) {
             if (seqbody.OWNED() != null && seqbody.col_name != null) {
                 sequence.setOwnedBy(getFullCtxText(seqbody.col_name));
+                String colName = getTableName(seqbody.col_name);
+                String tableName = getTableName(seqbody.col_name);
+                String schName = getSchemaName(seqbody.col_name);
+                if (tableName.equals(schName)) {
+                    schName = schemaName;
+                }
+                addObjReference(schName, new PgTable(tableName, "" ,""), seqbody.col_name.getStart().getStartIndex());
             }
         }
         addObjReference(schemaName, sequence, ctx.name.getStart().getStartIndex());
