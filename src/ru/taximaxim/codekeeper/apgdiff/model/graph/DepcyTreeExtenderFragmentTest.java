982df2a8a333a470d3eedfb26640e2be62b8f0e3
ryabinin_av <ryabinin_av@taximaxim.ru>
2015-01-12 17:18:25 +0300

    DepcyTreeExtenderTest for ANTLR

--- src/ru/taximaxim/codekeeper/apgdiff/model/graph/DepcyTreeExtenderFragmentTest.java
+++ src/ru/taximaxim/codekeeper/apgdiff/model/graph/DepcyTreeExtenderFragmentTest.java
@@ -1,8 +1,10 @@
 package ru.taximaxim.codekeeper.apgdiff.model.graph;
 
+import cz.startnet.utils.pgdiff.loader.ParserClass;
+
 public class DepcyTreeExtenderFragmentTest extends DepcyTreeExtenderTest {
 
-    public DepcyTreeExtenderFragmentTest(int fileIndex) {
-        super(fileIndex);
+    public DepcyTreeExtenderFragmentTest(TreeElementCreator predefined, ParserClass parserType) {
+        super(predefined, parserType);
     }
 }
