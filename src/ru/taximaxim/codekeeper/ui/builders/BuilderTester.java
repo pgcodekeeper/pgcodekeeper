c7bd818f172b98677d7170cc9fff3d7ec5912d4d
Ботов Анатолий <botov_av@taximaxim.ru>
2015-01-15 07:32:22 +0300

    Add builder, remove builder appearing

---
+++ src/ru/taximaxim/codekeeper/ui/builders/BuilderTester.java
@@ -0,0 +1,26 @@
+package ru.taximaxim.codekeeper.ui.builders;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.Platform;
+
+import ru.taximaxim.codekeeper.ui.handlers.AddBuilder;
+
+public class BuilderTester extends PropertyTester {
+
+    private static final String IS_ENABLED = "isEnabled";
+
+    @Override
+    public boolean test(Object receiver, String property, Object[] args,
+            Object expectedValue) {
+        if (IS_ENABLED.equals(property)) {
+            final IProject project = (IProject) Platform.getAdapterManager()
+                    .getAdapter(receiver, IProject.class);
+
+            if (project != null)
+                return AddBuilder.hasBuilder(project);
+        }
+
+        return false;
+    }
+}
