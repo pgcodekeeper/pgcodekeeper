4ec9029ae6645b2b287cb131f37fb27da5ac60de
Ботов Анатолий <botov_av@taximaxim.ru>
2015-02-18 14:34:43 +0300

    [Fix] Использовать '/* */' конструкцию для определения комментария.

--- src/ru/taximaxim/codekeeper/ui/sqledit/SQLEditorCommonDocumentProvider.java
+++ src/ru/taximaxim/codekeeper/ui/sqledit/SQLEditorCommonDocumentProvider.java
@@ -50,7 +50,7 @@ public class SQLEditorCommonDocumentProvider {
     IDocumentPartitioner createRecipePartitioner() {
         IPredicateRule[] rules= { 
                 new SingleLineRule("--", null, new Token(SQL_SINGLE_COMMENT), (char) 0, true, false), //$NON-NLS-1$
-                new MultiLineRule("/**", "*/", new Token(SQL_MULTI_COMMENT), (char) 0, true),  //$NON-NLS-1$ //$NON-NLS-2$
+                new MultiLineRule("/*", "*/", new Token(SQL_MULTI_COMMENT), (char) 0, true),  //$NON-NLS-1$ //$NON-NLS-2$
                 new SingleLineRule( "'", "'", new Token(SQL_CHARACTER_STRING_LITERAL), (char) 0 ) //$NON-NLS-1$ //$NON-NLS-2$
                 };
 
