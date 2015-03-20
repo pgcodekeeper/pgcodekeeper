af5d2dc660820315fe03a1fc308ab74cf22a59c1
Ботов Анатолий <botov_av@taximaxim.ru>
2015-03-20 13:05:31 +0300

    [New] Кнопка в представлении консоли codekeeper показывать консоль.

---
+++ src/ru/taximaxim/codekeeper/ui/consoles/ShowConsoleParticipant.java
@@ -0,0 +1,86 @@
+package ru.taximaxim.codekeeper.ui.consoles;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleConstants;
+import org.eclipse.ui.console.IConsolePageParticipant;
+import org.eclipse.ui.part.IPageBookViewPage;
+
+import ru.taximaxim.codekeeper.ui.Activator;
+import ru.taximaxim.codekeeper.ui.UIConsts.FILE;
+import ru.taximaxim.codekeeper.ui.UIConsts.PREF;
+import ru.taximaxim.codekeeper.ui.localizations.Messages;
+
+public class ShowConsoleParticipant implements IConsolePageParticipant {
+
+    ShowConsoleAction faction;
+    IPreferenceStore prefs = Activator.getDefault().getPreferenceStore();
+    @Override
+    public Object getAdapter(Class adapter) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void init(IPageBookViewPage page, IConsole console) {
+        if (faction== null) {
+            faction = new ShowConsoleAction(prefs);
+        }
+        page.getSite().getActionBars().getToolBarManager()
+                .appendToGroup(IConsoleConstants.OUTPUT_GROUP, faction);
+        prefs.addPropertyChangeListener(faction);
+    }
+
+    @Override
+    public void dispose() {
+        prefs.removePropertyChangeListener(faction);
+        prefs = null;
+    }
+
+    @Override
+    public void activated() {
+        
+    }
+
+    @Override
+    public void deactivated() {
+
+    }
+    
+    private static class ShowConsoleAction extends Action implements IPropertyChangeListener {
+        private IPreferenceStore prefs;
+        ShowConsoleAction(IPreferenceStore prefs) {
+            super(Messages.generalPrefPage_show_console_when_program_write_to_console);
+            setToolTipText(Messages.generalPrefPage_show_console_when_program_write_to_console);
+            setHoverImageDescriptor(ImageDescriptor.createFromURL(
+                    Activator.getContext().getBundle().getResource(FILE.ICONWRITEOUTCONSOLE)));
+            setDisabledImageDescriptor(ImageDescriptor.createFromURL(
+                    Activator.getContext().getBundle().getResource(FILE.ICONWRITEOUTCONSOLE)));
+            setImageDescriptor(ImageDescriptor.createFromURL(
+                    Activator.getContext().getBundle().getResource(FILE.ICONWRITEOUTCONSOLE)));
+            this.prefs = prefs;
+            setChecked(prefs.getBoolean(PREF.FORCE_SHOW_CONSOLE));
+//          PlatformUI.getWorkbench().getHelpSystem().setHelp(this, IConsoleHelpContextIds.CLEAR_CONSOLE_ACTION);
+        }
+
+        @Override
+        public void propertyChange(PropertyChangeEvent event) {
+            if (event.getProperty().equals(PREF.FORCE_SHOW_CONSOLE)
+                    && !event.getOldValue().equals(event.getNewValue())) {
+                setChecked((boolean)event.getNewValue()); 
+            }
+        }
+        @Override
+        public void run() {
+            prefs.setValue(PREF.FORCE_SHOW_CONSOLE, isChecked());
+        }
+    }
+
+
+}
+
+
