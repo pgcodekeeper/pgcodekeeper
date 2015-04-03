071436112ddc772ed3ded70a03ce691f2056d5e6
Ботов Анатолий <botov_av@taximaxim.ru>
2015-04-03 14:37:59 +0300

    Добавлены тесты на наследование таблиц, они требуют доработки.

---
+++ src/main/resources/cz/startnet/utils/pgdiff/depcies/table_inherits_del_t1_new.sql
@@ -0,0 +1,69 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: t2; Type: TABLE; Schema: public; Owner: botov_av; Tablespace: 
+--
+
+CREATE TABLE t2 (
+    c1 integer,
+    c2 integer
+);
+
+
+ALTER TABLE public.t2 OWNER TO botov_av;
+
+--
+-- Name: v1; Type: VIEW; Schema: public; Owner: botov_av
+--
+
+CREATE VIEW v1 AS
+ SELECT t2.c1,
+    t2.c2
+   FROM t2;
+
+
+ALTER TABLE public.v1 OWNER TO botov_av;
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
