1159debbc0854b3451eea3d41297dbbfbbe1382b
Ботов Анатолий <botov_av@taximaxim.ru>
2015-05-18 10:52:53 +0300

    [Fix] tablespace для index только в запросе, нужно расширение PGIndex.

--- apgdiff/src/main/resources/cz/startnet/utils/pgdiff/loader/QUERY_INDICES_PER_SCHEMA.sql
+++ apgdiff/src/main/resources/cz/startnet/utils/pgdiff/loader/QUERY_INDICES_PER_SCHEMA.sql
@@ -7,12 +7,15 @@ SELECT ccc.relname AS table_name,
      i.indisclustered as isClustered,
     c.relowner,
     d.description AS comment,
+    tabsp.spcname as table_space,
     definition
 FROM pg_catalog.pg_class ccc
 RIGHT JOIN pg_catalog.pg_index i ON ccc.oid = i.indrelid
 LEFT JOIN pg_catalog.pg_description d ON i.indexrelid = d.objoid
     AND d.objsubid = 0
+LEFT JOIN pg_tablespace tabsp ON tabsp.oid = i.indexrelid
 JOIN pg_catalog.pg_class c ON c.oid = i.indexrelid
+LEFT JOIN pg_catalog.pg_tablespace tabsp ON tabsp.oid = c.reltablespace
 LEFT JOIN pg_catalog.pg_constraint cons ON cons.conindid = i.indexrelid,
 pg_get_indexdef(c.oid) definition
 WHERE ccc.relkind = 'r'
