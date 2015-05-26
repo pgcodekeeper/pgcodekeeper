da2c96b336b94bf76c039d26239d556c69e8a537
Левша Александр <levsha_aa@taximaxim.ru>
2015-05-26 16:59:30 +0300

    fixed maven scripts

--- script/deploy-no-tests.sh
+++ script/deploy-no-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-(cd "${DIR}/../ru.taximaxim.codeKeeper/ru.taximaxim.codekeeper.mainapp/" && mvn clean deploy -P default,deploy-standalone -DskipTests=true)
+(cd "${DIR}/../ru.taximaxim.codekeeper.mainapp/ru.taximaxim.codeKeeper/" && mvn clean deploy -P default,deploy-standalone -DskipTests=true)
