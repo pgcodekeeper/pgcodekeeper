bd088942a0a524f31a79684a703f8f6458958ee8
Левша Александр <levsha_aa@taximaxim.ru>
2015-06-25 17:55:10 +0300

    перевел операции с URI на URIUtil; считаем, что URL никогда не URL-закодирован

--- apgdiff/src/main/java/ru/taximaxim/codekeeper/apgdiff/ApgdiffUtils.java
+++ apgdiff/src/main/java/ru/taximaxim/codekeeper/apgdiff/ApgdiffUtils.java
@@ -2,29 +2,21 @@ package ru.taximaxim.codekeeper.apgdiff;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLDecoder;
 
 import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.URIUtil;
 
 public class ApgdiffUtils {
 
-    public static File getFileFromOsgiRes(URL url) throws URISyntaxException, IOException{
-        URI uri;
-        if (!url.getProtocol().equals("file")){
-            url = FileLocator.toFileURL(url);
-        }
-        uri = new URI(url.getProtocol(), 
-                null /*userInfo*/,
-                url.getHost(), 
-                url.getPort(), 
-                (url.getPath()==null)?null:URLDecoder.decode(url.getPath(), ApgdiffConsts.UTF_8),
-                (url.getQuery()==null)?null:URLDecoder.decode(url.getQuery(), ApgdiffConsts.UTF_8),
-                null /*fragment*/);
-        
-        return new File(uri);
+    /**
+     * @param url url should NOT be URL-encoded
+     */
+    public static File getFileFromOsgiRes(URL url) throws URISyntaxException, IOException {
+        return new File(
+                URIUtil.toURI(url.getProtocol().equals("file") ?
+                        url : FileLocator.toFileURL(url)));
     }
 
     private ApgdiffUtils() {
