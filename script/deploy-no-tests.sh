c9696f6dc43e92c0dcd03216527d791c8a8d315e
Левша Александр <levsha_aa@taximaxim.ru>
2015-10-02 15:55:30 +0300

    deploying to pgcodekeeper.ru

--- script/deploy-no-tests.sh
+++ script/deploy-no-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-(cd "${DIR}/../ru.taximaxim.codekeeper.mainapp/ru.taximaxim.codeKeeper/" && mvn clean deploy -P default,deploy-standalone -DskipTests)
+(cd "${DIR}/../ru.taximaxim.codekeeper.mainapp/ru.taximaxim.codeKeeper/" && mvn clean deploy -P default,deploy-standalone,deploy-public -DskipTests)
