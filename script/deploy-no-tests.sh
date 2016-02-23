8c9377c9e4406f490ddd5be9d2af60b828be9491
Левша Александр <levsha_aa@taximaxim.ru>
2016-02-23 18:32:04 +0300

    обфускация выполняется всегда, кроме профиля test; профиль deploy-cli-public

--- script/deploy-no-tests.sh
+++ script/deploy-no-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-(cd "${DIR}/../ru.taximaxim.codekeeper.mainapp/ru.taximaxim.codeKeeper/" && mvn clean deploy -P default,deploy-standalone,deploy-public -DskipTests)
+(cd "${DIR}/../ru.taximaxim.codekeeper.mainapp/ru.taximaxim.codeKeeper/" && mvn clean deploy -P default,deploy-cli,deploy-public,deploy-cli-public -DskipTests)
