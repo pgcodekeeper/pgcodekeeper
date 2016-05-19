b5494d10090b9d0d5842a3d458ca5e7730d9ef6a
Левша Александр <levsha_aa@taximaxim.ru>
2016-05-19 09:21:28 +0300

    fixed non-left join with pg_description for rules

--- apgdiff/src/main/resources/cz/startnet/utils/pgdiff/loader/QUERY_RULES_PER_SCHEMA.sql
+++ apgdiff/src/main/resources/cz/startnet/utils/pgdiff/loader/QUERY_RULES_PER_SCHEMA.sql
@@ -9,7 +9,7 @@ SELECT  ccc.relname,
 		d.description as comment
 FROM pg_catalog.pg_rewrite r
 JOIN pg_catalog.pg_class ccc ON ccc.oid = r.ev_class 
-JOIN pg_catalog.pg_description d ON r.oid = d.objoid
+LEFT JOIN pg_catalog.pg_description d ON r.oid = d.objoid
 WHERE ccc.relnamespace = ? AND
     -- block rules that implement views
     NOT ((ccc.relkind = 'v' OR ccc.relkind = 'm') AND r.ev_type = '1' AND r.is_instead)
