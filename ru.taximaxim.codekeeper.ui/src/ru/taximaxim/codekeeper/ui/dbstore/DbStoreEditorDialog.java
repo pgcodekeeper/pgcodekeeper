6ebf521f11bb74e9276c878c282d64e34006af69
Левша Александр <levsha_aa@taximaxim.ru>
2016-12-22 17:20:31 +0300

    DbPicker удален, интергрирован в DbStoreEditorDialog; исправлен баг этим UI

--- ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/dbstore/DbStoreEditorDialog.java
+++ ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/dbstore/DbStoreEditorDialog.java
@@ -4,22 +4,35 @@ import java.text.MessageFormat;
 
 import org.eclipse.jface.dialogs.TrayDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.ShellAdapter;
 import org.eclipse.swt.events.ShellEvent;
+import org.eclipse.swt.events.VerifyEvent;
+import org.eclipse.swt.events.VerifyListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.ISharedImages;
 
+import ru.taximaxim.codekeeper.ui.Activator;
 import ru.taximaxim.codekeeper.ui.localizations.Messages;
 
 public class DbStoreEditorDialog extends TrayDialog {
 
     private final DbInfo dbInitial;
     private DbInfo dbInfo;
-    private DbPicker grpDbData;
+    private Group grpDbData;
+
+    private Text txtName, txtDbName, txtDbUser, txtDbPass, txtDbHost, txtDbPort;
+    private CLabel lblWarnDbPass;
 
     public DbInfo getDbInfo(){
         return dbInfo;
@@ -38,13 +51,17 @@ public class DbStoreEditorDialog extends TrayDialog {
 
             @Override
             public void shellActivated(ShellEvent e) {
-                // do pack-calling methods after open has returned
-                // otherwise shell is opened at (0,0) coordinates
-
-                // one-time listener, remove after first execution
+                // one time listener
                 newShell.removeShellListener(this);
 
-                grpDbData.setStoreEditMode();
+                if (dbInitial != null){
+                    txtName.setText(dbInitial.getName());
+                    txtDbName.setText(dbInitial.getDbName());
+                    txtDbUser.setText(dbInitial.getDbUser());
+                    txtDbPass.setText(dbInitial.getDbPass());
+                    txtDbHost.setText(dbInitial.getDbHost());
+                    txtDbPort.setText("" + dbInitial.getDbPort()); //$NON-NLS-1$
+                }
             }
         });
     }
@@ -53,27 +70,79 @@ public class DbStoreEditorDialog extends TrayDialog {
     protected Control createDialogArea(Composite parent) {
         Composite area = (Composite) super.createDialogArea(parent);
 
-        Composite container = new Composite(area, SWT.NONE);
-        GridLayout gridLayout = new GridLayout();
-        gridLayout.marginWidth = 0;
-        gridLayout.marginHeight = 0;
-        container.setLayout(gridLayout);
-        container.setLayoutData(new GridData(GridData.FILL_BOTH));
-
-        grpDbData = new DbPicker(container, SWT.NONE, null);
+        grpDbData = new Group(area, SWT.NONE);
+        grpDbData.setText(Messages.dbStoreEditorDialog_db_info);
+        grpDbData.setLayout(new GridLayout(2, false));
         GridData gd = new GridData(SWT.FILL, SWT.FILL, true, true);
         gd.widthHint = 480;
         grpDbData.setLayoutData(gd);
-        grpDbData.setText(Messages.dbStoreEditorDialog_db_info);
 
-        if (dbInitial != null){
-            grpDbData.getTxtName().setText(dbInitial.getName());
-            grpDbData.getTxtDbName().setText(dbInitial.getDbName());
-            grpDbData.getTxtDbUser().setText(dbInitial.getDbUser());
-            grpDbData.getTxtDbPass().setText(dbInitial.getDbPass());
-            grpDbData.getTxtDbHost().setText(dbInitial.getDbHost());
-            grpDbData.getTxtDbPort().setText("" + dbInitial.getDbPort()); //$NON-NLS-1$
-        }
+        new Label(grpDbData, SWT.NONE).setText(Messages.entry_name);
+
+        txtName = new Text(grpDbData, SWT.BORDER);
+        txtName.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+        new Label(grpDbData, SWT.NONE).setText(Messages.dB_name);
+
+        txtDbName = new Text(grpDbData, SWT.BORDER);
+        txtDbName.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+        new Label(grpDbData, SWT.NONE).setText(Messages.dB_user);
+
+        txtDbUser = new Text(grpDbData, SWT.BORDER);
+        txtDbUser.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+        new Label(grpDbData, SWT.NONE).setText(Messages.dB_password);
+
+        txtDbPass = new Text(grpDbData, SWT.BORDER | SWT.PASSWORD);
+        txtDbPass.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+        txtDbPass.addModifyListener(new ModifyListener() {
+
+            @Override
+            public void modifyText(ModifyEvent e) {
+                GridData gd = (GridData) lblWarnDbPass.getLayoutData();
+
+                if((txtDbPass.getText().isEmpty() && !gd.exclude)
+                        || (!txtDbPass.getText().isEmpty() && gd.exclude)) {
+                    lblWarnDbPass.setVisible(!lblWarnDbPass.getVisible());
+                    gd.exclude = !gd.exclude;
+
+                    getShell().pack();
+                }
+            }
+        });
+
+        lblWarnDbPass = new CLabel(grpDbData, SWT.NONE);
+        lblWarnDbPass.setImage(Activator.getEclipseImage(ISharedImages.IMG_OBJS_WARN_TSK));
+        lblWarnDbPass.setText(Messages.warning_providing_password_here_is_insecure_use_pgpass_instead);
+        gd = new GridData(SWT.FILL, SWT.FILL, false, false, 2, 1);
+        gd.exclude = true;
+        lblWarnDbPass.setLayoutData(gd);
+        lblWarnDbPass.setVisible(false);
+
+        new Label(grpDbData, SWT.NONE).setText(Messages.dB_host);
+
+        txtDbHost = new Text(grpDbData, SWT.BORDER);
+        txtDbHost.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+        new Label(grpDbData, SWT.NONE).setText(Messages.dbPicker_port);
+
+        txtDbPort = new Text(grpDbData, SWT.BORDER);
+        gd = new GridData(60, SWT.DEFAULT);
+        txtDbPort.setLayoutData(gd);
+        txtDbPort.addVerifyListener(new VerifyListener() {
+
+            @Override
+            public void verifyText(VerifyEvent e) {
+                try{
+                    if (!e.text.isEmpty() && Integer.valueOf(e.text) < 0){
+                        e.doit = false;
+                    }
+                }catch(NumberFormatException ex){
+                    e.doit = false;
+                }
+            }
+        });
 
         return area;
     }
@@ -81,25 +150,26 @@ public class DbStoreEditorDialog extends TrayDialog {
     @Override
     protected void okPressed() {
         int dbport;
-        try {
-            if(grpDbData.getTxtDbPort().getText().isEmpty()) {
-                dbport = 0;
-            } else {
-                dbport = Integer.parseInt(grpDbData.getTxtDbPort().getText());
+        String port = txtDbPort.getText();
+        if(txtDbPort.getText().isEmpty()) {
+            dbport = 0;
+        } else {
+            try {
+                dbport = Integer.parseInt(port);
+            } catch (NumberFormatException ex) {
+                MessageBox mb = new MessageBox(getShell(), SWT.ICON_ERROR);
+                mb.setText(Messages.dbStoreEditorDialog_cannot_save_entry);
+                mb.setMessage(MessageFormat.format(
+                        Messages.dbStoreEditorDialog_not_valid_port_number,
+                        port));
+                mb.open();
+                return;
             }
-        } catch (NumberFormatException ex) {
-            MessageBox mb = new MessageBox(getShell(), SWT.ICON_ERROR);
-            mb.setText(Messages.dbStoreEditorDialog_cannot_save_entry);
-            mb.setMessage(MessageFormat.format(
-                    Messages.dbStoreEditorDialog_not_valid_port_number,
-                    grpDbData.getTxtDbPort().getText()));
-            mb.open();
-            return;
         }
 
-        dbInfo = new DbInfo(grpDbData.getTxtName().getText(), grpDbData.getTxtDbName().getText(),
-                grpDbData.getTxtDbUser().getText(), grpDbData.getTxtDbPass().getText(),
-                grpDbData.getTxtDbHost().getText(), dbport);
+        dbInfo = new DbInfo(txtName.getText(), txtDbName.getText(),
+                txtDbUser.getText(), txtDbPass.getText(),
+                txtDbHost.getText(), dbport);
         super.okPressed();
     }
 }
