54233b5dda4a5f6de9c4c868124d36b9f5885921
galiev_mr <galiev_mr@taximaxim.ru>
2017-03-13 15:01:31 +0300

    Hidden schemas created by extensions

--- apgdiff/src/main/resources/cz/startnet/utils/pgdiff/loader/QUERY_SCHEMAS.sql
+++ apgdiff/src/main/resources/cz/startnet/utils/pgdiff/loader/QUERY_SCHEMAS.sql
@@ -7,3 +7,4 @@ FROM pg_catalog.pg_namespace n
 LEFT JOIN pg_catalog.pg_description d ON n.oid = d.objoid
 WHERE n.nspname NOT LIKE 'pg\_%'
     AND n.nspname != 'information_schema'
+    AND NOT EXISTS (SELECT 1 FROM pg_catalog.pg_depend dp WHERE dp.objid = n.oid AND dp.deptype = 'e')
