bc1633e1b760793ed44f028962624a5a533586ae
galiev_mr <galiev_mr@taximaxim.ru>
2017-03-15 17:34:06 +0300

    added test for exclude constraint

---
+++ apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/add_exclude_constraint_diff.sql
@@ -0,0 +1,5 @@
+ALTER TABLE testtable
+	ADD CONSTRAINT testtable2_c_excl EXCLUDE USING gist (c WITH &&);
+
+ALTER TABLE testtable
+	ADD CONSTRAINT test EXCLUDE USING id(test WITH omg) INITIALLY DEFERRED;
