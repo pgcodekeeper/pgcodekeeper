a0f3cdabcd31b33c7574db3ba5343c71ec1f8173
Шамсутдинов Ленар Ришадович <shamsutdinov_lr@taximaxim.local>
2017-07-12 11:02:19 +0300

    add operations for quick update button (write to the DB and compare with the DB)

--- ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/handlers/QuickUpdate.java
+++ ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/handlers/QuickUpdate.java
@@ -1,30 +1,52 @@
- 
+
 package ru.taximaxim.codekeeper.ui.handlers;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.jface.wizard.WizardDialog;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 
-import ru.taximaxim.codekeeper.ui.Activator;
 import ru.taximaxim.codekeeper.ui.Log;
-import ru.taximaxim.codekeeper.ui.pgdbproject.DiffWizard;
-import ru.taximaxim.codekeeper.ui.pgdbproject.PgDbProject;
+import ru.taximaxim.codekeeper.ui.editors.ProjectEditorDiffer;
+import ru.taximaxim.codekeeper.ui.sqledit.RollOnEditor;
 
 public class QuickUpdate extends AbstractHandler {
 
     @Override
     public Object execute(ExecutionEvent event) {
-        PgDbProject proj = OpenProjectUtils.getProject(event);
-        Shell shell = HandlerUtil.getActiveShell(event);
-        IPreferenceStore prefStore = Activator.getDefault().getPreferenceStore();
-        Log.log(Log.LOG_DEBUG, "Diff wizard about to show"); //$NON-NLS-1$
-            
-        WizardDialog dialog = new WizardDialog(
-                shell, new DiffWizard(proj, prefStore));
-        dialog.open();
+        IWorkbench wb = PlatformUI.getWorkbench();
+        IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
+        IWorkbenchPage page = win.getActivePage();
+        IEditorPart editor = page.getActiveEditor();
+
+        RollOnEditor rollOnEditor;
+        if(editor instanceof RollOnEditor){
+            rollOnEditor = (RollOnEditor)editor;
+        } else {
+            return null;
+        }
+
+        Log.log(Log.LOG_DEBUG, "QuickUpdate starting"); //$NON-NLS-1$
+
+        if(rollOnEditor.runQuickUpdate()){
+            IEditorReference[] editorsReferences = page.getEditorReferences();
+
+            for(IEditorReference editorsReference : editorsReferences){
+                if("ru.taximaxim.codekeeper.ui.projectEditorDiffer".equals(editorsReference.getId())){
+                    IEditorPart projectEditor = editorsReference.getEditor(true);
+                    if(projectEditor != null){
+                        ProjectEditorDiffer projectEditorDiffer = (ProjectEditorDiffer)projectEditor;
+                        projectEditorDiffer.getChanges();
+                        page.activate(projectEditorDiffer);
+                        break;
+                    }
+                }
+            }
+        }
         return null;
     }
 }
