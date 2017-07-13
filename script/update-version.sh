4f0406d8822c2ef2afa28050a8c17eb8be05af6f
Левша Александр <levsha_aa@taximaxim.ru>
2017-07-13 11:36:38 +0300

    версионирование через tycho-versions-plugin

---
+++ script/update-version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+( \
+cd "${DIR}/../ru.taximaxim.codekeeper.mainapp/ru.taximaxim.codeKeeper/" \
+&& \
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version \
+-Dartifacts=\
+ru.taximaxim.codekeeper.standalone.product,\
+ru.taximaxim.codekeeper.updatesite,\
+ru.taximaxim.codekeeper.mainapp,\
+ru.taximaxim.codekeeper.feature \
+-DnewVersion=$1"-SNAPSHOT" \
+)
