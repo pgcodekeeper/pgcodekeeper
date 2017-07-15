a70fe060b738eeea9c4998d5ae0c43a8421d76b2
Левша Александр <levsha_aa@taximaxim.ru>
2017-07-15 19:20:20 +0300

    use --allowed-object as GUI-style "user selection"

--- apgdiff/src/main/java/cz/startnet/utils/pgdiff/PgDiff.java
+++ apgdiff/src/main/java/cz/startnet/utils/pgdiff/PgDiff.java
@@ -168,6 +168,7 @@ public final class PgDiff {
         List<TreeElement> selected = new TreeFlattener()
                 .onlySelected()
                 .useIgnoreList(ignoreList)
+                .onlyTypes(arguments.getAllowedTypes())
                 .flatten(root);
         //TODO----------КОСТЫЛЬ колонки добавляются как выбранные если выбрана таблица-----------
         addColumnsAsElements(oldDbFull, newDbFull, selected);
