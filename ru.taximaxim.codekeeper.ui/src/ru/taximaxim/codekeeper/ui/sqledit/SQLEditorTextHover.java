293d6d4169d94e556ece31b9d7ca72f156109134
galiev_mr <galiev_mr@taximaxim.ru>
2017-08-15 09:28:59 +0300

    added multiline text hover

--- ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/sqledit/SQLEditorTextHover.java
+++ ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/sqledit/SQLEditorTextHover.java
@@ -2,17 +2,23 @@ package ru.taximaxim.codekeeper.ui.sqledit;
 
 import java.util.List;
 
+import org.eclipse.jface.text.DefaultInformationControl;
 import org.eclipse.jface.text.DefaultTextHover;
+import org.eclipse.jface.text.IInformationControl;
+import org.eclipse.jface.text.IInformationControlCreator;
 import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITextHoverExtension;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.Region;
 import org.eclipse.jface.text.source.Annotation;
 import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.editors.text.EditorsUI;
 
 import cz.startnet.utils.pgdiff.schema.PgObjLocation;
 import ru.taximaxim.codekeeper.ui.pgdbproject.parser.PgDbParser;
 
-final class SQLEditorTextHover extends DefaultTextHover {
+final class SQLEditorTextHover extends DefaultTextHover implements ITextHoverExtension  {
 
     private static final String QUICKDIFF = "org.eclipse.ui.workbench.texteditor.quickdiff"; //$NON-NLS-1$
 
@@ -39,6 +45,16 @@ final class SQLEditorTextHover extends DefaultTextHover {
             }
         }
         return new Region(offset, 0);
+    }
+
+    @Override
+    public IInformationControlCreator getHoverControlCreator() {
+        return new IInformationControlCreator() {
+            @Override
+            public IInformationControl createInformationControl(Shell parent) {
+                return new DefaultInformationControl(parent, EditorsUI.getTooltipAffordanceString());
+            }
+        };
     }
 
     @Override
