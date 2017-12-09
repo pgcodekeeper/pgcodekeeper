097468505edb0b37d7d1ac03ec3ecd25670b4d54
Левша Александр <levsha_aa@taximaxim.ru>
2017-12-09 18:16:59 +0300

    removed leftover plpgsql extensions from tests

--- apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/depcies/move_obj_to_schema_new.sql
+++ apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/depcies/move_obj_to_schema_new.sql
@@ -27,14 +27,14 @@ ALTER SCHEMA test OWNER TO galiev_mr;
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+--CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
 
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+--COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
 SET search_path = test, pg_catalog;
