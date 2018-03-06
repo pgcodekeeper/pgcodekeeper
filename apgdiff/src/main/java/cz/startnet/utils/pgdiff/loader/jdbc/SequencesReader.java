0d769bdc96d06f75add9c101f44c6d6235a7d628
galiev_mr <galiev_mr@taximaxim.ru>
2018-03-06 15:09:22 +0300

    fixed postgres 10 identity columns

--- apgdiff/src/main/java/cz/startnet/utils/pgdiff/loader/jdbc/SequencesReader.java
+++ apgdiff/src/main/java/cz/startnet/utils/pgdiff/loader/jdbc/SequencesReader.java
@@ -57,7 +57,8 @@ public class SequencesReader extends JdbcReader {
             identityType = res.getString("attidentity");
         }
 
-        if (refTable != null && identityType == null) {
+        if (refTable != null && (identityType == null
+                || (!"d".equals(identityType) && !"a".equals(identityType)))) {
             s.setOwnedBy(PgDiffUtils.getQuotedName(refTable) + '.'
                     + PgDiffUtils.getQuotedName(res.getString("ref_col_name")));
         }
@@ -84,7 +85,7 @@ public class SequencesReader extends JdbcReader {
             }
         }
 
-        if (identityType != null) {
+        if ("d".equals(identityType) || "a".equals(identityType)) {
             PgTable table = schema.getTable(refTable);
             PgColumn column = table.getColumn(refColumn);
             if (column == null) {
