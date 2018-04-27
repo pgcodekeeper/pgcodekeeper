7a06773d3a0b00971b8fb7cc08873c4912d2c988
galiev_mr <galiev_mr@taximaxim.ru>
2018-04-27 09:25:00 +0300

    Fixed children order in tests

--- apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/drop_trigger_diff.sql
+++ apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/drop_trigger_diff.sql
@@ -1,5 +1,5 @@
 SET search_path = public, pg_catalog;
 
-DROP TRIGGER test_view_trigger1 ON test_view;
-
 DROP TRIGGER test_table_trigger ON test_table;
+
+DROP TRIGGER test_view_trigger1 ON test_view;
