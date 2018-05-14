9656697664c29c6d085d1a27bac19c59809af60b
Левша Александр <levsha_aa@taximaxim.ru>
2018-05-14 11:10:50 +0300

    version 4.5.1
    removed obsolete scripts

--- script/license_decode.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -cp "${DIR}/../apgdiff/lib/license3j/*" License3j decode --license-file="$1" --keyring-file="${HOME}/.gnupg/pubring.gpg"
--- script/license_encode.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -cp "${DIR}/../apgdiff/lib/license3j/*" License3j encode --license-file="$1" --keyring-file="${HOME}/.gnupg/secring.gpg" --key="Technology, LLC (pgCodeKeeper) <ags@taximaxim.ru>" --password="" --output="$1.enc"
-
