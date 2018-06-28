1d01eee99223871ddcc70a105a900e33a5df831c
Шамсутдинов Ленар Ришадович <shamsutdinov_lr@taximaxim.ru>
2018-06-28 15:34:46 +0300

    fixed cli-tests

--- apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/MainTest_modify_column_type_diff.sql
+++ apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/MainTest_modify_column_type_diff.sql
@@ -2,15 +2,15 @@ SET TIMEZONE TO 'UTC';
 
 SET check_function_bodies = false;
 
+SET search_path = pg_catalog;
+
 START TRANSACTION;
 
-SET search_path = public, pg_catalog;
+ALTER TABLE public.testtable
+	ALTER COLUMN field1 TYPE integer USING field1::integer; /* TYPE change - table: public.testtable original: smallint new: integer */
 
-ALTER TABLE testtable
-	ALTER COLUMN field1 TYPE integer USING field1::integer; /* TYPE change - table: testtable original: smallint new: integer */
-
-ALTER TABLE testtable
-	ALTER COLUMN field3 TYPE character varying(150) USING field3::character varying(150); /* TYPE change - table: testtable original: character varying(100) new: character varying(150) */
+ALTER TABLE public.testtable
+	ALTER COLUMN field3 TYPE character varying(150) USING field3::character varying(150); /* TYPE change - table: public.testtable original: character varying(100) new: character varying(150) */
 
 COMMIT TRANSACTION;
 
--- apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/same_allowed_object_diff.sql
+++ apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/same_allowed_object_diff.sql
@@ -1,8 +1,7 @@
+SET search_path = pg_catalog;
+
 -- HIDDEN: Object public of type SCHEMA
 
 -- HIDDEN: Object public.testtable of type TABLE
 
-SET search_path = public, pg_catalog;
-
 CREATE INDEX testindex ON public.testtable USING btree (field3);
-
