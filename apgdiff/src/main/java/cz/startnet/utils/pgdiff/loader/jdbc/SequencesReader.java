1b8ad6b779900e153abb124469d52afa2e862bcd
Шамсутдинов Ленар Ришадович <shamsutdinov_lr@taximaxim.ru>
2018-07-17 09:34:44 +0300

    fixed quota for 'sequence-owned-by'

--- apgdiff/src/main/java/cz/startnet/utils/pgdiff/loader/jdbc/SequencesReader.java
+++ apgdiff/src/main/java/cz/startnet/utils/pgdiff/loader/jdbc/SequencesReader.java
@@ -62,7 +62,7 @@ public class SequencesReader extends JdbcReader {
         }
 
         if (refTable != null && identityType == null) {
-            s.setOwnedBy(refTable + '.' + res.getString("ref_col_name"));
+            s.setOwnedBy(refTable + '.' + PgDiffUtils.getQuotedName(res.getString("ref_col_name")));
         }
 
         if (identityType == null) {
