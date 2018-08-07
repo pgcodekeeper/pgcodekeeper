76e99cc6ad156b87a7cfc8651ca2d2af46de7241
galiev_mr <galiev_mr@taximaxim.ru>
2018-08-07 13:30:33 +0300

    fixed identity change test

--- apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/modify_ms_table_identity_diff.sql
+++ apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/modify_ms_table_identity_diff.sql
@@ -1,7 +1,7 @@
-ALTER TABLE [testtable]
+ALTER TABLE [dbo].[testtable]
 	DROP COLUMN c1
 GO
 
-ALTER TABLE [testtable]
+ALTER TABLE [dbo].[testtable]
 	ADD [c1] [int] NOT NULL IDENTITY (2,1) NOT FOR REPLICATION
 GO
