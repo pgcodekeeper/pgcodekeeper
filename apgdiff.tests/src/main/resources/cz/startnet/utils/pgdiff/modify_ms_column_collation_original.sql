4e3d4282f338551fb84ba3df5e860bbde0f67269
galiev_mr <galiev_mr@taximaxim.ru>
2018-09-27 09:11:26 +0300

    moved go to basic keywords, fixed test

--- apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/modify_ms_column_collation_original.sql
+++ apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/modify_ms_column_collation_original.sql
@@ -4,6 +4,6 @@ SET ANSI_NULLS ON
 GO
 CREATE TABLE [dbo].[testtable] (
     [c1] [integer] NOT NULL,
-    [c2] [varchar] (15) COLLATE Latin1_General_CI_AS NOT NULL,
+    [c2] [varchar] (15) COLLATE Latin1_General_CI_AS NOT NULL
 )
 GO
