6fb9b7372c34eab7a1fed42e4479f2735d9f1884
Шамсутдинов Ленар Ришадович <shamsutdinov_lr@taximaxim.ru>
2018-11-28 07:57:59 +0300

    Fixed incorrect query.

--- apgdiff/src/main/resources/cz/startnet/utils/pgdiff/loader/QUERY_AGGREGATES_VERSION_11.sql
+++ apgdiff/src/main/resources/cz/startnet/utils/pgdiff/loader/QUERY_AGGREGATES_VERSION_11.sql
@@ -3,3 +3,4 @@ SELECT  p.oid::bigint,
         a.aggfinalmodify AS finalfunc_modify,
         a.aggmfinalmodify AS mfinalfunc_modify
 FROM pg_catalog.pg_proc p
+LEFT JOIN pg_catalog.pg_aggregate a ON a.aggfnoid = p.oid
