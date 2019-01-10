b71712b0da8af1263dc26e5fb58b8eb0cbb402b5
galiev_mr <galiev_mr@taximaxim.ru>
2019-01-10 09:37:45 +0300

    fixed expr tests

--- apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/parsers/antlr/expr/compare_types_aster_ord_view_new.sql
+++ apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/parsers/antlr/expr/compare_types_aster_ord_view_new.sql
@@ -1,180 +1,197 @@
-SET TIMEZONE TO 'UTC';
-
-SET check_function_bodies = false;
-
-SET search_path = public, pg_catalog;
-
-CREATE OR REPLACE FUNCTION dup3(integer) RETURNS TABLE(f1 integer, f2 double precision)
+CREATE FUNCTION public.dup3(integer) RETURNS TABLE(f1 integer, f2 double precision)
     LANGUAGE sql
     AS $_$ SELECT $1, $1::double precision $_$;
 
-ALTER FUNCTION dup3(integer) OWNER TO shamsutdinov_lr;
-
-CREATE OR REPLACE FUNCTION f(p integer) RETURNS integer
+CREATE FUNCTION public.f(p integer) RETURNS integer
     LANGUAGE plpgsql
     AS $_$begin return $1; end;$_$;
 
-ALTER FUNCTION f(p integer) OWNER TO shamsutdinov_lr;
-
-CREATE OR REPLACE FUNCTION f(s text) RETURNS text
+CREATE FUNCTION public.f(s text) RETURNS text
     LANGUAGE plpgsql
     AS $$begin return 'textttt'; end;$$;
 
-ALTER FUNCTION f(s text) OWNER TO shamsutdinov_lr;
+CREATE TABLE public.mytable (
+    col111 integer,
+    col222 integer
+);
 
---------------------------------------------------------------------------------
+CREATE TABLE public.one1 (
+    col11 text
+);
 
-CREATE TABLE tablettt (
+CREATE TABLE public.one2 (
+    col22 integer
+);
+
+CREATE TABLE public.tablettt (
     c1 integer,
     c2 text,
     c4 text,
     c3 double precision
 );
 
-ALTER TABLE tablettt OWNER TO shamsutdinov_lr;
-
-CREATE TABLE table_inherits (
-    own_column integer
-) INHERITS (tablettt);
-
-ALTER TABLE table_inherits OWNER TO shamsutdinov_lr;
-
-CREATE TABLE users (
+CREATE TABLE public.users (
     id integer,
     login character(64),
     password character(64)
 );
 
-ALTER TABLE users OWNER TO shamsutdinov_lr;
+CREATE VIEW public.asterisk AS
+ WITH mmyytable AS (
+         SELECT mytable.col111,
+            mytable.col222,
+            111 AS myvalue
+           FROM public.mytable
+        )
+ SELECT t.c1,
+    t.c2,
+    t.c4,
+    t.c3,
+    u.id,
+    u.login,
+    u.password,
+    mt.col111,
+    mt.col222,
+    mt.myvalue,
+    a.col11,
+    b.col22
+   FROM public.tablettt t,
+    public.users u,
+    mmyytable mt,
+    ( SELECT one1.col11
+           FROM public.one1
+         LIMIT 1) a,
+    ( SELECT one2.col22
+           FROM public.one2
+         LIMIT 1) b;
 
-CREATE TABLE one1 (
-    col11 text
-);
+CREATE VIEW public.asterisk1 AS
+ SELECT tablettt.c1,
+    tablettt.c2,
+    tablettt.c4,
+    tablettt.c3,
+    users.id,
+    users.login,
+    users.password
+   FROM public.tablettt,
+    public.users;
 
-ALTER TABLE one1 OWNER TO shamsutdinov_lr;
+CREATE VIEW public.asterisk2 AS
+ SELECT t.c1,
+    t.c2,
+    t.c4,
+    t.c3,
+    u.id,
+    u.login,
+    u.password
+   FROM public.tablettt t,
+    public.users u;
 
-CREATE TABLE one2 (
-    col22 integer
-);
+CREATE VIEW public.asterisk3 AS
+ SELECT f.f
+   FROM public.f(3) f(f);
 
-ALTER TABLE one2 OWNER TO shamsutdinov_lr;
-
-CREATE TABLE mytable (
-    col111 integer,
-    col222 integer
-);
-
-ALTER TABLE mytable OWNER TO shamsutdinov_lr;
-    
---------------------------------------------------------------------------------
-
-CREATE VIEW asterisk AS
-    WITH mmyytable AS (
-    SELECT *, 111 AS myvalue FROM mytable
-    )
-    SELECT * FROM 
-        tablettt t, 
-        users u,
-        mmyytable mt,
-        (SELECT * FROM one1 LIMIT 1) a,
-        (SELECT * FROM one2 LIMIT 1) b;
-
-ALTER VIEW asterisk OWNER TO shamsutdinov_lr;
-
-CREATE VIEW asterisk1 AS
-    SELECT tablettt.*, users.*
-   FROM tablettt, users;
-
-ALTER VIEW asterisk1 OWNER TO shamsutdinov_lr;
-
-CREATE VIEW asterisk2 AS
-    SELECT t.*, u.*
-   FROM tablettt t, users u;
-
-ALTER VIEW asterisk2 OWNER TO shamsutdinov_lr;
-
-CREATE VIEW asterisk3 AS
-    SELECT *
-   FROM 
-   f(3);
-
-ALTER VIEW asterisk3 OWNER TO shamsutdinov_lr;
-
-CREATE VIEW asterisk4 AS
-    SELECT d.*, t.*
-   FROM 
-   f(3) d, f('txt') t;
-
-ALTER VIEW asterisk4 OWNER TO shamsutdinov_lr;
+CREATE VIEW public.asterisk4 AS
+ SELECT d.d,
+    t.t
+   FROM public.f(3) d(d),
+    public.f('txt'::text) t(t);
 
 /*
 TODO for realize in future
 
-CREATE VIEW asterisk5 AS
-    SELECT d.*
-   FROM 
-   dup3(3) d;
+CREATE VIEW public.asterisk5 AS
+ SELECT d.f1,
+    d.f2
+   FROM public.dup3(3) d(f1, f2);
 
-ALTER VIEW asterisk5 OWNER TO shamsutdinov_lr;
-
-CREATE VIEW asterisk6 AS
-    SELECT *
-   FROM 
-   dup3(3);
-
-ALTER VIEW asterisk6 OWNER TO shamsutdinov_lr;
+CREATE VIEW public.asterisk6 AS
+ SELECT dup3.f1,
+    dup3.f2
+   FROM public.dup3(3) dup3(f1, f2);
 */
 
-CREATE VIEW asterisk7 AS
-    SELECT q.*
-   FROM (SELECT t.*, u.* FROM tablettt t, users u) q;
+CREATE VIEW public.asterisk7 AS
+ SELECT q.c1,
+    q.c2,
+    q.c4,
+    q.c3,
+    q.id,
+    q.login,
+    q.password
+   FROM ( SELECT t.c1,
+            t.c2,
+            t.c4,
+            t.c3,
+            u.id,
+            u.login,
+            u.password
+           FROM public.tablettt t,
+            public.users u) q;
 
-ALTER VIEW asterisk7 OWNER TO shamsutdinov_lr;
+CREATE VIEW public.asterisk8 AS
+ SELECT ( SELECT one1.col11
+           FROM public.one1
+         LIMIT 1) AS onee,
+    ( SELECT one2.col22
+           FROM public.one2
+         LIMIT 1) AS twoo;
 
-CREATE VIEW asterisk8 AS
-    SELECT
-        (SELECT * FROM one1 LIMIT 1) AS onee,
-        (SELECT * FROM one2 LIMIT 1) AS twoo;
+CREATE VIEW public.asterisk9 AS
+ SELECT ( SELECT one1.col11
+           FROM public.one1
+         LIMIT 1) AS col11,
+    ( SELECT one2.col22
+           FROM public.one2
+         LIMIT 1) AS col22;
 
-ALTER VIEW asterisk8 OWNER TO shamsutdinov_lr;
+CREATE VIEW public.asterisk10 AS
+ SELECT s.col11
+   FROM public.one1 s
+ LIMIT 1;
 
-CREATE VIEW asterisk9 AS
-    SELECT
-        (SELECT * FROM one1 LIMIT 1),
-        (SELECT * FROM one2 LIMIT 1);
+CREATE VIEW public.asterisk11 AS
+ SELECT a.col11,
+    b.col22
+   FROM ( SELECT one1.col11
+           FROM public.one1
+         LIMIT 1) a,
+    ( SELECT one2.col22
+           FROM public.one2
+         LIMIT 1) b;
 
-ALTER VIEW asterisk9 OWNER TO shamsutdinov_lr;
+CREATE TABLE public.table_inherits (
+    own_column integer
+)
+INHERITS (public.tablettt);
 
-CREATE VIEW asterisk10 AS
-        SELECT s.* FROM one1 s LIMIT 1;
+CREATE VIEW public.asterisk12 AS
+ SELECT r.c1,
+    r.c2,
+    r.c4,
+    r.c3,
+    r.own_column
+   FROM ( SELECT table_inherits.c1,
+            table_inherits.c2,
+            table_inherits.c4,
+            table_inherits.c3,
+            table_inherits.own_column
+           FROM public.table_inherits) r;
 
-ALTER VIEW asterisk10 OWNER TO shamsutdinov_lr;
+CREATE VIEW public.asterisk13 AS
+ SELECT pg_cast.castsource,
+    pg_cast.casttarget,
+    pg_cast.castfunc,
+    pg_cast.castcontext,
+    pg_cast.castmethod
+   FROM pg_cast;
+ 
+CREATE VIEW public.asterisk14 AS
+ SELECT version.version
+   FROM version() version(version);
 
-CREATE VIEW asterisk11 AS
-    SELECT * FROM
-        (SELECT * FROM one1 LIMIT 1) a,
-        (SELECT * FROM one2 LIMIT 1) b;
-
-ALTER VIEW asterisk11 OWNER TO shamsutdinov_lr;
-
-CREATE VIEW asterisk12 AS
-    select * from (select * from table_inherits) r;
-
-ALTER VIEW asterisk12 OWNER TO shamsutdinov_lr;
-
-CREATE VIEW asterisk13 AS
-    select * from pg_cast;
-
-ALTER VIEW asterisk13 OWNER TO shamsutdinov_lr;
-
-CREATE VIEW asterisk14 AS
-    select * from version();
-
-ALTER VIEW asterisk14 OWNER TO shamsutdinov_lr;
-
-CREATE VIEW asterisk15 AS
-    SELECT
-    (select count(*) from tablettt) as qqq,
-    (select to_json(d.*) from tablettt d) as www;
-
-ALTER VIEW asterisk15 OWNER TO shamsutdinov_lr;
+CREATE VIEW public.asterisk15 AS
+ SELECT ( SELECT count(*) AS count
+           FROM public.tablettt) AS qqq,
+    ( SELECT to_json(d.*) AS to_json
+           FROM public.tablettt d) AS www;
