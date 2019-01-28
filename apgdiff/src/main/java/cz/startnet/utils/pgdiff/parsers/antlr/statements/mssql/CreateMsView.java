d5df45ce06e7f95bd6a87be086866dfc9bd4d8d8
galiev_mr <galiev_mr@taximaxim.ru>
2019-01-28 10:15:52 +0300

    removed override

--- apgdiff/src/main/java/cz/startnet/utils/pgdiff/parsers/antlr/statements/mssql/CreateMsView.java
+++ apgdiff/src/main/java/cz/startnet/utils/pgdiff/parsers/antlr/statements/mssql/CreateMsView.java
@@ -1,11 +1,7 @@
 package cz.startnet.utils.pgdiff.parsers.antlr.statements.mssql;
 
-import java.util.List;
-
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.Token;
-import org.antlr.v4.runtime.misc.Interval;
 
 import cz.startnet.utils.pgdiff.parsers.antlr.TSQLParser.Batch_statementContext;
 import cz.startnet.utils.pgdiff.parsers.antlr.TSQLParser.Create_or_alter_viewContext;
@@ -13,15 +9,12 @@ import cz.startnet.utils.pgdiff.parsers.antlr.TSQLParser.IdContext;
 import cz.startnet.utils.pgdiff.parsers.antlr.TSQLParser.Select_statementContext;
 import cz.startnet.utils.pgdiff.parsers.antlr.msexpr.MsSelect;
 import cz.startnet.utils.pgdiff.parsers.antlr.rulectx.MsSelectStmt;
-import cz.startnet.utils.pgdiff.parsers.antlr.statements.ParserAbstract;
 import cz.startnet.utils.pgdiff.schema.AbstractSchema;
 import cz.startnet.utils.pgdiff.schema.MsView;
 import cz.startnet.utils.pgdiff.schema.PgDatabase;
-import cz.startnet.utils.pgdiff.schema.SourceStatement;
 
 public class CreateMsView extends BatchContextProcessor {
 
-    private boolean isSchemaDef;
     private final Create_or_alter_viewContext ctx;
 
     private final boolean ansiNulls;
@@ -37,11 +30,10 @@ public class CreateMsView extends BatchContextProcessor {
 
     public CreateMsView(Create_or_alter_viewContext ctx, PgDatabase db,
             boolean ansiNulls, boolean quotedIdentifier, CommonTokenStream stream) {
-        super(db, null, stream);
+        super(db, ctx.getParent(), stream);
         this.ctx = ctx;
         this.ansiNulls = ansiNulls;
         this.quotedIdentifier = quotedIdentifier;
-        isSchemaDef = true;
     }
 
     @Override
@@ -72,25 +64,5 @@ public class CreateMsView extends BatchContextProcessor {
         }
 
         return view;
-    }
-
-    @Override
-    protected void setSourceParts(SourceStatement st) {
-        if (!isSchemaDef) {
-            super.setSourceParts(st);
-            return;
-        }
-
-        ParserRuleContext viewCtxWithCreate = ctx.getParent();
-        boolean isKeepNewLines = db.getArguments().isKeepNewlines();
-        String first = ParserAbstract.getHiddenLeftCtxText(viewCtxWithCreate, stream);
-        st.setFirstPart(isKeepNewLines ? first : first.replace("\r", ""));
-
-        List<Token> endTokens = stream.getHiddenTokensToRight(viewCtxWithCreate.getStop().getTokenIndex());
-        Token stopToken = endTokens != null ? endTokens.get(endTokens.size() - 1) : viewCtxWithCreate.getStop();
-        int stop = stopToken.getStopIndex();
-        int start = getDelimiterCtx().getStop().getStopIndex() + 1;
-        String second = stopToken.getInputStream().getText(Interval.of(start, stop));
-        st.setSecondPart(isKeepNewLines ? second : second.replace("\r", ""));
     }
 }
