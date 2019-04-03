14b0e72c72434e4451993fce991e7b09ce1f946b
Шамсутдинов Ленар Ришадович <shamsutdinov_lr@taximaxim.ru>
2019-04-03 11:47:49 +0300

    fixed showing the flags for enabled SQLEditor templates

--- ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/prefs/SQLEditorTemplatePrefPage.java
+++ ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/prefs/SQLEditorTemplatePrefPage.java
@@ -1,5 +1,10 @@
 package ru.taximaxim.codekeeper.ui.prefs;
 
+import java.util.Arrays;
+
+import org.eclipse.jface.viewers.CheckboxTableViewer;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.text.templates.TemplatePersistenceData;
 import org.eclipse.ui.texteditor.templates.TemplatePreferencePage;
 
 import ru.taximaxim.codekeeper.ui.Activator;
@@ -15,9 +20,17 @@ public class SQLEditorTemplatePrefPage extends TemplatePreferencePage {
                     .getTemplateStore());
             setContextTypeRegistry(SQLEditorTemplateManager.getInstance()
                     .getContextTypeRegistry());
+            setFlagsToEnabledTemplates();
         } catch (Exception ex) {
             Log.log(Log.LOG_ERROR, "Cannot get sql templates", ex); //$NON-NLS-1$
         }
+    }
+
+    private void setFlagsToEnabledTemplates() {
+        Display.getDefault().asyncExec(() -> Arrays
+                .stream(((CheckboxTableViewer) getTableViewer()).getTable().getItems())
+                .filter(i -> ((TemplatePersistenceData) i.getData()).isEnabled())
+                .forEach(i -> i.setChecked(true)));
     }
 
     @Override
