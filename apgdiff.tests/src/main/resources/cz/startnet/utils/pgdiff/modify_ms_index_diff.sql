b99802ec08fea5dfbf5d72bcec6cee49fe56509d
Левша Александр <levsha_aa@taximaxim.ru>
2019-04-25 16:29:59 +0300

    use DROP_EXISTING for MsIndex alters

--- apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/modify_ms_index_diff.sql
+++ apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/modify_ms_index_diff.sql
@@ -1,5 +1,3 @@
-DROP INDEX [index_table1] ON [dbo].[table1]
-GO
-
 CREATE UNIQUE NONCLUSTERED INDEX [index_table1] ON [dbo].[table1] ([c1] ASC, [c2] DESC)
+WITH (DROP_EXISTING = ON)
 GO
--- apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/modify_ms_index_option_diff.sql
+++ apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/modify_ms_index_option_diff.sql
@@ -1,7 +1,4 @@
-DROP INDEX [index_table1] ON [dbo].[table1]
-GO
-
 CREATE NONCLUSTERED INDEX [index_table1] ON [dbo].[table1] ([c1])
-WITH (FILLFACTOR = 50)
+WITH (FILLFACTOR = 50, DROP_EXISTING = ON)
 ON [PRIMARY]
 GO
