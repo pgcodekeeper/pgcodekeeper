d82a4153f1eded0f22fe94fee732f1c835453bbd
Шамсутдинов Ленар Ришадович <shamsutdinov_lr@taximaxim.ru>
2019-05-16 14:07:22 +0300

    added protection from editing and deleting for templates of pgCodeKeeper

--- ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/prefs/SQLEditorTemplatePrefPage.java
+++ ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/prefs/SQLEditorTemplatePrefPage.java
@@ -1,5 +1,13 @@
 package ru.taximaxim.codekeeper.ui.prefs;
 
+import java.util.Arrays;
+
+import org.eclipse.jface.text.templates.persistence.TemplatePersistenceData;
+import org.eclipse.jface.text.templates.persistence.TemplateStore;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.texteditor.templates.TemplatePreferencePage;
 
 import ru.taximaxim.codekeeper.ui.Activator;
@@ -23,5 +31,33 @@ public class SQLEditorTemplatePrefPage extends TemplatePreferencePage {
     @Override
     protected boolean isShowFormatterSetting() {
         return false;
+    }
+
+    @Override
+    protected Control createContents(Composite ancestor) {
+        Control control = super.createContents(ancestor);
+        getTableViewer().setContentProvider(new OnlyUserTemplatesContentProvider());
+        return control;
+    }
+
+    private class OnlyUserTemplatesContentProvider implements IStructuredContentProvider {
+
+        private TemplateStore fStore;
+
+        @Override
+        public Object[] getElements(Object input) {
+            return Arrays.stream(fStore.getTemplateData(false))
+                    .filter(TemplatePersistenceData::isUserAdded).toArray();
+        }
+
+        @Override
+        public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+            fStore= (TemplateStore) newInput;
+        }
+
+        @Override
+        public void dispose() {
+            fStore= null;
+        }
     }
 }
