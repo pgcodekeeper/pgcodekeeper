83dd7f512699d421bf0b68f4dd6d6ea23ea2a103
galiev_mr <galiev_mr@taximaxim.ru>
2019-09-16 16:07:41 +0300

    fixed command visibility

--- ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/handlers/OpenSQLEditor.java
+++ ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/handlers/OpenSQLEditor.java
@@ -6,17 +6,13 @@ import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.filesystem.IFileStore;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.ide.IDE;
 
 import ru.taximaxim.codekeeper.ui.UIConsts.EDITOR;
 import ru.taximaxim.codekeeper.ui.dialogs.ExceptionNotifier;
-import ru.taximaxim.codekeeper.ui.editors.ProjectEditorDiffer;
-import ru.taximaxim.codekeeper.ui.sqledit.SQLEditor;
 import ru.taximaxim.codekeeper.ui.sqledit.SQLEditorInput;
 
 public class OpenSQLEditor extends AbstractHandler {
@@ -35,11 +31,5 @@ public class OpenSQLEditor extends AbstractHandler {
             ExceptionNotifier.notifyDefault(e.getLocalizedMessage(), e);
         }
         return null;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        IEditorPart editor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
-        return editor instanceof ProjectEditorDiffer || editor instanceof SQLEditor;
     }
 }
