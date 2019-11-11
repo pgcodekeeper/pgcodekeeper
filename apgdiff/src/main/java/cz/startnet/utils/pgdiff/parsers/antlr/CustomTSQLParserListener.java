44ae1a3adbedb2428eca5c461ddfdb95d5cee5ae
galiev_mr <galiev_mr@taximaxim.ru>
2019-11-11 13:18:55 +0300

    removed legacy code

--- apgdiff/src/main/java/cz/startnet/utils/pgdiff/parsers/antlr/CustomTSQLParserListener.java
+++ apgdiff/src/main/java/cz/startnet/utils/pgdiff/parsers/antlr/CustomTSQLParserListener.java
@@ -11,7 +11,6 @@ import cz.startnet.utils.pgdiff.parsers.antlr.TSQLParser.Another_statementContex
 import cz.startnet.utils.pgdiff.parsers.antlr.TSQLParser.BatchContext;
 import cz.startnet.utils.pgdiff.parsers.antlr.TSQLParser.Batch_statementContext;
 import cz.startnet.utils.pgdiff.parsers.antlr.TSQLParser.Batch_statement_bodyContext;
-import cz.startnet.utils.pgdiff.parsers.antlr.TSQLParser.Create_or_alter_viewContext;
 import cz.startnet.utils.pgdiff.parsers.antlr.TSQLParser.Ddl_clauseContext;
 import cz.startnet.utils.pgdiff.parsers.antlr.TSQLParser.Dml_clauseContext;
 import cz.startnet.utils.pgdiff.parsers.antlr.TSQLParser.Enable_disable_triggerContext;
@@ -135,11 +134,6 @@ implements TSqlContextProcessor {
             return;
         }
 
-        safeParseStatement(p, ctx);
-    }
-
-    public void schemaViewStatement(Create_or_alter_viewContext ctx, CommonTokenStream stream) {
-        ParserAbstract p = new CreateMsView(ctx, db, ansiNulls, quotedIdentifier, stream);
         safeParseStatement(p, ctx);
     }
 
