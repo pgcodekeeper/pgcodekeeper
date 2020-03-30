cd0f4c477a946482edabbd2a4299cf38d63c9820
Шамсутдинов Ленар Ришадович <shamsutdinov_lr@taximaxim.ru>
2020-03-30 15:16:45 +0300

    fixed code formatting

--- ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/sqledit/Segments.java
+++ ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/sqledit/Segments.java
@@ -9,53 +9,53 @@ import ru.taximaxim.codekeeper.apgdiff.model.difftree.DbObjType;
 
 public class Segments extends Position {
 
-	private final String name;
-	private final DbObjType type;
-	private final String action;
+    private final String name;
+    private final DbObjType type;
+    private final String action;
 
-	/**
-	 * Creates a new segment covering the given range.
-	 *
-	 * @param offset the offset of the segment
-	 * @param length the length of the segment
-	 */
-	public Segments(PgObjLocation loc) {
-		super(loc.getOffset(), loc.getObjLength());
-		this.name = loc.getObjName();
-		this.type = loc.getType();
-		this.action = loc.getAction();
-	}
+    /**
+     * Creates a new segment covering the given range.
+     *
+     * @param offset the offset of the segment
+     * @param length the length of the segment
+     */
+    public Segments(PgObjLocation loc) {
+        super(loc.getOffset(), loc.getObjLength());
+        this.name = loc.getObjName();
+        this.type = loc.getType();
+        this.action = loc.getAction();
+    }
 
-	public DbObjType getType() {
-		return type;
-	}
+    public DbObjType getType() {
+        return type;
+    }
 
-	@Override
-	public String toString() {
-		return action + ' ' + name;
-	}
+    @Override
+    public String toString() {
+        return action + ' ' + name;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + Objects.hash(action, name, type);
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + Objects.hash(action, name, type);
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (!super.equals(obj)) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		Segments other = (Segments) obj;
-		return Objects.equals(action, other.action)
-				&& Objects.equals(name, other.name) && type == other.type;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Segments other = (Segments) obj;
+        return Objects.equals(action, other.action)
+                && Objects.equals(name, other.name) && type == other.type;
+    }
 }
