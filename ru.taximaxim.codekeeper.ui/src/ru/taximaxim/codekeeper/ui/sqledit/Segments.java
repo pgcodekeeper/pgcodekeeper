c6dc20e560f6f564f532b7fa3fff29709e71bf38
lshamsutdinov <l.shamsutdinov@mail.ru>
2020-03-30 14:52:40 +0300

    fixed NPE in navigation

--- ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/sqledit/Segments.java
+++ ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/sqledit/Segments.java
@@ -1,5 +1,7 @@
 package ru.taximaxim.codekeeper.ui.sqledit;
 
+import java.util.Objects;
+
 import org.eclipse.jface.text.Position;
 
 import cz.startnet.utils.pgdiff.schema.PgObjLocation;
@@ -7,67 +9,53 @@ import ru.taximaxim.codekeeper.apgdiff.model.difftree.DbObjType;
 
 public class Segments extends Position {
 
-    private final String name;
-    private final DbObjType type;
-    private final String action;
+	private final String name;
+	private final DbObjType type;
+	private final String action;
 
-    /**
-     * Creates a new segment covering the given range.
-     *
-     * @param offset the offset of the segment
-     * @param length the length of the segment
-     */
-    public Segments(PgObjLocation loc) {
-        super(loc.getOffset(), loc.getObjLength());
-        this.name = loc.getObjName();
-        this.type = loc.getType();
-        this.action = loc.getAction();
-    }
+	/**
+	 * Creates a new segment covering the given range.
+	 *
+	 * @param offset the offset of the segment
+	 * @param length the length of the segment
+	 */
+	public Segments(PgObjLocation loc) {
+		super(loc.getOffset(), loc.getObjLength());
+		this.name = loc.getObjName();
+		this.type = loc.getType();
+		this.action = loc.getAction();
+	}
 
-    public DbObjType getType() {
-        return type;
-    }
+	public DbObjType getType() {
+		return type;
+	}
 
-    @Override
-    public String toString() {
-        return action + ' ' + name;
-    }
+	@Override
+	public String toString() {
+		return action + ' ' + name;
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = super.hashCode();
-        result = prime * result + ((action == null) ? 0 : action.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + ((type == null) ? 0 : type.hashCode());
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(action, name, type);
+		return result;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (!super.equals(obj)) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        Segments other = (Segments) obj;
-        if (action == null && other.action != null) {
-            return false;
-        } else if (!action.equals(other.action)) {
-            return false;
-        }
-        if (name == null && other.name != null) {
-            return false;
-        } else if (!name.equals(other.name)) {
-            return false;
-        }
-        if (type != other.type) {
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!super.equals(obj)) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Segments other = (Segments) obj;
+		return Objects.equals(action, other.action)
+				&& Objects.equals(name, other.name) && type == other.type;
+	}
 }
--- ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/sqledit/SegmentsWithParent.java
+++ ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/sqledit/SegmentsWithParent.java
@@ -1,5 +1,7 @@
 package ru.taximaxim.codekeeper.ui.sqledit;
 
+import java.util.Objects;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IAdaptable;
 
@@ -7,50 +9,45 @@ import cz.startnet.utils.pgdiff.schema.PgObjLocation;
 
 public class SegmentsWithParent extends Segments implements IAdaptable {
 
-    private final IFile parentFile;
+	private final IFile parentFile;
 
-    public SegmentsWithParent(PgObjLocation loc, IFile parentFile) {
-        super(loc);
-        this.parentFile = parentFile;
-    }
+	public SegmentsWithParent(PgObjLocation loc, IFile parentFile) {
+		super(loc);
+		this.parentFile = parentFile;
+	}
 
-    public IFile getParentFile() {
-        return parentFile;
-    }
+	public IFile getParentFile() {
+		return parentFile;
+	}
 
-    @Override
-    public <T> T getAdapter(Class<T> adapter) {
-        if (adapter.isAssignableFrom(IFile.class)) {
-            return adapter.cast(parentFile);
-        }
-        return null;
-    }
+	@Override
+	public <T> T getAdapter(Class<T> adapter) {
+		if (adapter.isAssignableFrom(IFile.class)) {
+			return adapter.cast(parentFile);
+		}
+		return null;
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = super.hashCode();
-        result = prime * result + ((parentFile == null) ? 0 : parentFile.hashCode());
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(parentFile);
+		return result;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (!super.equals(obj)) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        SegmentsWithParent other = (SegmentsWithParent) obj;
-        if (parentFile == null && other.parentFile != null) {
-            return false;
-        } else if (!parentFile.equals(other.parentFile)) {
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!super.equals(obj)) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		SegmentsWithParent other = (SegmentsWithParent) obj;
+		return Objects.equals(parentFile, other.parentFile);
+	}
 }
