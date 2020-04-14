b2a1f605b2591137dfe5fb4d266373e57fe8843e
Левша Александр <levsha_aa@taximaxim.ru>
2020-04-14 16:43:23 +0300

    moved PreferenceStore into earlyStartup methods; l10n
    Activator/PreferenceStores broke if acquired in IStartup initialization

--- ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/pgdbproject/parser/ParserCacheCleaner.java
+++ ru.taximaxim.codekeeper.ui/src/ru/taximaxim/codekeeper/ui/pgdbproject/parser/ParserCacheCleaner.java
@@ -18,24 +18,23 @@ public class ParserCacheCleaner implements IStartup {
 
     private static final long CHECK_INTERVAL = 30000;
 
-    private final IPreferenceStore mainPrefs = Activator.getDefault().getPreferenceStore();
-
     @Override
     public void earlyStartup() {
+        IPreferenceStore mainPrefs = Activator.getDefault().getPreferenceStore();
         Thread thread = new Thread(() -> {
             while (true) {
-                long cleaningInterval = TimeUnit.MINUTES
-                        .toMillis(mainPrefs.getInt(PREF.PARSER_CACHE_CLEANING_INTERVAL));
-                if (cleaningInterval != 0) {
-                    AntlrParser.checkToClean(false, cleaningInterval);
-                    AntlrParser.checkToClean(true, cleaningInterval);
-                }
-
                 try {
                     Thread.sleep(CHECK_INTERVAL);
                 } catch (InterruptedException e) {
                     Log.log(e);
                     Thread.currentThread().interrupt();
+                }
+
+                long cleaningInterval = TimeUnit.MINUTES
+                        .toMillis(mainPrefs.getInt(PREF.PARSER_CACHE_CLEANING_INTERVAL));
+                if (cleaningInterval != 0) {
+                    AntlrParser.checkToClean(false, cleaningInterval);
+                    AntlrParser.checkToClean(true, cleaningInterval);
                 }
             }
         });
