03cae715477bafaad96c241828ca2e5de11eaa4e
Шамсутдинов Ленар Ришадович <shamsutdinov_lr@taximaxim.ru>
2020-07-20 09:53:52 +0300

    corrected test for use reserved keywords without quotes in names

--- apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/add_tbl_and_func_with_keyword_name_no_quotes_diff.sql
+++ apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/add_tbl_and_func_with_keyword_name_no_quotes_diff.sql
@@ -9,7 +9,7 @@ CREATE OR REPLACE FUNCTION public."user"() RETURNS integer
 ALTER FUNCTION public."user"() OWNER TO shamsutdinov_lr;
 
 CREATE TABLE public."user" (
-	c1 integer DEFAULT public."user"()
+	c1 integer DEFAULT public.user()
 );
 
 ALTER TABLE public."user" OWNER TO shamsutdinov_lr;
