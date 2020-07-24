f593a0d6c137f2ee19cdcc0525b819ba0bf9fa6e
Шамсутдинов Ленар Ришадович <shamsutdinov_lr@taximaxim.ru>
2020-07-24 11:22:48 +0300

    corrected CLI tests

--- apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/script_from_objs_with_difference_diff.sql
+++ apgdiff.tests/src/main/resources/cz/startnet/utils/pgdiff/script_from_objs_with_difference_diff.sql
@@ -5,8 +5,9 @@ SET search_path = pg_catalog;
 -- HIDDEN: Object public.v1 of type VIEW (action DROP)
 
 ALTER TABLE public.tbl
-    ALTER COLUMN name TYPE text USING name::text; /* ТИП колонки изменился - Таблица: public.tbl оригинал: integer новый: text */
+	ALTER COLUMN name TYPE integer USING name::integer; /* TYPE change - table: public.tbl original: text new: integer */
 
 -- HIDDEN: Object public.v1 of type VIEW (action CREATE)
 
 -- HIDDEN: Object public.v2 of type VIEW (action CREATE)
+
