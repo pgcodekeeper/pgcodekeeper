22b429911e25791add990cc7c034e24811ee616d
galiev_mr <galiev_mr@taximaxim.ru>
2020-08-03 16:50:55 +0300

    changed name validation, little refactoring

--- apgdiff/src/main/java/ru/taximaxim/codekeeper/apgdiff/fileutils/FileUtils.java
+++ apgdiff/src/main/java/ru/taximaxim/codekeeper/apgdiff/fileutils/FileUtils.java
@@ -17,8 +17,6 @@ import cz.startnet.utils.pgdiff.PgDiffUtils;
 
 public final class FileUtils {
 
-    private static final int HASH_LENGTH = 10;
-
     private static final DateTimeFormatter FILE_DATE = DateTimeFormatter.ofPattern("yyyy-MM-dd HH''mm''ss");
     private static final Pattern INVALID_FILENAME = Pattern.compile("[\\\\/:*?\"<>|]");
 
@@ -35,7 +33,7 @@ public final class FileUtils {
                 throw wrapEx.getCause();
             }
         }
-        FileUtils.removeReadOnly(f);
+        removeReadOnly(f);
     }
 
     public static void removeReadOnly(Path path) throws IOException {
@@ -58,19 +56,16 @@ public final class FileUtils {
     }
 
     public static String getValidFilename(String name) {
-        Matcher m = FileUtils.INVALID_FILENAME.matcher(name);
+        Matcher m = INVALID_FILENAME.matcher(name);
         if (m.find()) {
-            String hash = PgDiffUtils.md5(name)
-                    // 2^40 variants, should be enough for this purpose
-                    .substring(0, HASH_LENGTH);
-            return m.replaceAll("") + '_' + hash; //$NON-NLS-1$
+            return m.replaceAll("_"); //$NON-NLS-1$
         } else {
             return name;
         }
     }
 
     public static String getFileDate() {
-        return FileUtils.FILE_DATE.format(LocalDateTime.now());
+        return FILE_DATE.format(LocalDateTime.now());
     }
 
     private FileUtils() {
