eca5add533305fbdb11458eaf42db85cb3dcdfb3
tavturin_ma <tavturin_ma@technology45.ru>
2020-12-03 11:19:08 +0300

    Added colletion in classes
    JdbcLoader and MetaUtils

--- apgdiff/src/main/java/cz/startnet/utils/pgdiff/loader/JdbcLoader.java
+++ apgdiff/src/main/java/cz/startnet/utils/pgdiff/loader/JdbcLoader.java
@@ -10,6 +10,7 @@ import org.eclipse.core.runtime.SubMonitor;
 import cz.startnet.utils.pgdiff.PgDiffArguments;
 import cz.startnet.utils.pgdiff.PgDiffUtils;
 import cz.startnet.utils.pgdiff.loader.jdbc.CastsReader;
+import cz.startnet.utils.pgdiff.loader.jdbc.CollationReader;
 import cz.startnet.utils.pgdiff.loader.jdbc.ConstraintsReader;
 import cz.startnet.utils.pgdiff.loader.jdbc.ExtensionsReader;
 import cz.startnet.utils.pgdiff.loader.jdbc.FtsConfigurationsReader;
@@ -92,6 +93,7 @@ public class JdbcLoader extends JdbcLoaderBase {
 
             new ExtensionsReader(this, d).read();
             new CastsReader(this, d).read();
+            new CollationReader(this).read();
 
             if (!SupportedVersion.VERSION_10.isLE(version)) {
                 SequencesReader.querySequencesData(d, this);
