ce0ce6084e7e7d1445886ba72b9ee19cf373dde6
Хазиева Гульназ <khazieva_gr@taximaxim.ru>
2022-09-22 16:58:58 +0300

    Added tests for cli mode

--- apgdiff.tests/src/main/java/cz/startnet/utils/pgdiff/PgDiffTest.java
+++ apgdiff.tests/src/main/java/cz/startnet/utils/pgdiff/PgDiffTest.java
@@ -526,6 +526,8 @@ public class PgDiffTest {
                     {"add_column_and_change_table"},
                     // Tests scenario where view column comments is reordered.
                     {"change_view_column_comment_order"},
+                    // Tests scenario where not valid in constraint printed.
+                    {"generate_constraint_not_valid"},
                 });
     }
 
