6890c3bfa533b4f976f58e1e0c70be9ff9890f93
Mansur Galiev <galiev_mr@taximaxim.ru>
2023-03-15 15:01:01 +0300

    added apache license 2.0 appendix to files in tests

--- ru.taximaxim.codekeeper.core.tests/src/main/java/ru/taximaxim/codekeeper/core/parsers/antlr/expr/ExprTypeTest.java
+++ ru.taximaxim.codekeeper.core.tests/src/main/java/ru/taximaxim/codekeeper/core/parsers/antlr/expr/ExprTypeTest.java
@@ -1,8 +1,22 @@
-/**
+/*******************************************************************************
+ * Copyright 2017-2023 TAXTELECOM, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  * Copyright 2006 StartNet s.r.o.
  *
  * Distributed under MIT license
- */
+ *******************************************************************************/
 package ru.taximaxim.codekeeper.core.parsers.antlr.expr;
 
 import java.io.IOException;
@@ -68,7 +82,7 @@ class ExprTypeTest {
     void runCompare(String fileNameTemplate) throws IOException, InterruptedException {
         PgDiffArguments args = new PgDiffArguments();
         String typesForCompare = getRelationColumnsTypes(
-                    loadAndAnalyze(fileNameTemplate, args, FILES_POSTFIX.ORIGINAL_SQL));
+                loadAndAnalyze(fileNameTemplate, args, FILES_POSTFIX.ORIGINAL_SQL));
 
         runDiff(fileNameTemplate, args, typesForCompare);
     }
