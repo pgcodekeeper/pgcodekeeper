e4f11db9279e408ff5963edcc9256ebbcb1bc894
Elmir Shamsutdinov <shamsutdinov_er@taximaxim.ru>
2023-05-04 15:15:48 +0300

    updated test cases for PgParserTest and PgDiffTest

--- ru.taximaxim.codekeeper.core.tests/src/main/resources/ru/taximaxim/codekeeper/core/compare_servers_original.sql
+++ ru.taximaxim.codekeeper.core.tests/src/main/resources/ru/taximaxim/codekeeper/core/compare_servers_original.sql
@@ -13,3 +13,11 @@ CREATE SERVER test_server_4 FOREIGN DATA WRAPPER fdw1 OPTIONS (host 'foo', dbnam
 --OWNER--
 CREATE SERVER test_server_5 FOREIGN DATA WRAPPER fdw1;
 ALTER SERVER test_server_5 OWNER TO khazieva_gr;
+
+-- greenplum option
+CREATE SERVER foreign_server FOREIGN DATA WRAPPER postgres_fdw OPTIONS (
+    mpp_execute 'all segments',
+    host '192.83.123.89',
+    port '5432',
+    dbname 'foreign_db'
+);
--- ru.taximaxim.codekeeper.core.tests/src/main/resources/ru/taximaxim/codekeeper/core/parsers/server.sql
+++ ru.taximaxim.codekeeper.core.tests/src/main/resources/ru/taximaxim/codekeeper/core/parsers/server.sql
@@ -3,3 +3,14 @@ CREATE SERVER myserver FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host 'foo', db
 ALTER SERVER myserver OPTIONS (host 'host');
 
 DROP SERVER myserver;
+
+-- only for greenplum
+
+CREATE SERVER foreign_server FOREIGN DATA WRAPPER postgres_fdw OPTIONS (
+    mpp_execute 'all segments',
+    host '192.83.123.89',
+    port '5432',
+    dbname 'foreign_db'
+);
+
+ALTER SERVER foreign_server OPTIONS (SET mpp_execute 'master');
