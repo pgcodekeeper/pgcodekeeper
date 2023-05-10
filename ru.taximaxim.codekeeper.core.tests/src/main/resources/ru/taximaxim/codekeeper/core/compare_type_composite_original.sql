87002ba3127ee07c8aba39c12a221310caf5286e
Шамсутдинов Эльмир <shamsutdinov_er@taximaxim.ru>
2023-05-10 14:33:46 +0500

    PGCK-212 add logic to recreate 'composit type' when columns position is changed

---
+++ ru.taximaxim.codekeeper.core.tests/src/main/resources/ru/taximaxim/codekeeper/core/compare_type_composite_original.sql
@@ -0,0 +1,14 @@
+CREATE TYPE public.test_type_v1 AS (
+    c1 text,
+    c2 integer,
+    c3 bigint,
+    c4 integer
+);
+
+CREATE TYPE public.test_type_v2 AS (
+    c1 text,
+    c2 integer,
+    c3 bigint,
+    c4 integer
+);
+
