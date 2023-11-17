7b2657d41bf182ae8823977dbf3b9a027ceb6b33
Elmir Shamsutdinov <shamsutdinov_er@taximaxim.ru>
2023-11-17 11:58:30 +0300

    fixed bug

--- ru.taximaxim.codekeeper.core.tests/src/main/resources/ru/taximaxim/codekeeper/core/add_constraint_diff.sql
+++ ru.taximaxim.codekeeper.core.tests/src/main/resources/ru/taximaxim/codekeeper/core/add_constraint_diff.sql
@@ -2,3 +2,6 @@ SET search_path = pg_catalog;
 
 ALTER TABLE public.testtable
 	ADD CONSTRAINT field4check CHECK (((field4 > (-5.0)::double precision) AND (field4 < (5.0)::double precision)));
+
+ALTER TABLE public.test_un
+	ADD CONSTRAINT con_col1 UNIQUE (col1) INCLUDE (col1) WITH (fillfactor='10');
--- ru.taximaxim.codekeeper.core.tests/src/main/resources/ru/taximaxim/codekeeper/core/add_constraint_new.sql
+++ ru.taximaxim.codekeeper.core.tests/src/main/resources/ru/taximaxim/codekeeper/core/add_constraint_new.sql
@@ -6,3 +6,8 @@ CREATE TABLE public.testtable (
     "full" timestamp with time zone DEFAULT '2006-11-10 00:00:00+01'::timestamp with time zone NOT NULL,
     CONSTRAINT field4check CHECK (((field4 > (-5.0)::double precision) AND (field4 < (5.0)::double precision)))
 );
+
+CREATE TABLE public.test_un (
+col1 text,
+CONSTRAINT con_col1 UNIQUE (col1) INCLUDE (col1) with (fillfactor='10')
+);
