9223705e49f34505e5c81a0948727776bf53a7f0
Галиев Мансур <galiev_mr@taximaxim.ru>
2024-02-01 12:35:13 +0500

    PGCK-317 excluded ms view with SCHEMABINDING from refresh logic

---
+++ ru.taximaxim.codekeeper.core.tests/src/main/resources/ru/taximaxim/codekeeper/core/modify_ms_table_schemabinding_diff.sql
@@ -0,0 +1,29 @@
+-- DEPCY: This INDEX depends on the COLUMN: [dbo].[firms_test].[nameRus]
+
+DROP INDEX [PK_search_firms_test] ON [dbo].[v_search_firms_test]
+GO
+
+-- DEPCY: This VIEW depends on the COLUMN: [dbo].[firms_test].[nameRus]
+
+DROP VIEW [dbo].[v_search_firms_test]
+GO
+
+ALTER TABLE [dbo].[firms_test]
+	ALTER COLUMN [nameRus] NVARCHAR(80) NULL
+GO
+
+-- DEPCY: This VIEW is a dependency of INDEX: [dbo].[v_search_firms_test].[PK_search_firms_test]
+
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE VIEW [dbo].[v_search_firms_test]
+WITH SCHEMABINDING
+AS
+SELECT firmId, nameRus
+FROM dbo.firms_test
+GO
+
+CREATE UNIQUE CLUSTERED INDEX [PK_search_firms_test] ON [dbo].[v_search_firms_test] ([firmId])
+GO
